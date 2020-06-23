334c0d59ff702042dbb29e25f10d98a830aefd2d
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2020-06-24 05:29:45 +0900

    add motivation -Primitive Analysis #1

--- report/#0Render.r
+++ report/#0Render.r
@@ -9,8 +9,8 @@ options(repo = "https://cran.rstudio.com/")
 #'
 #+ Rmd File to Output
 rmarkdown::render(
-    input = "./Report/Powerpoint.Rmd",
-    output_file = "200705骨髄バンク中間報告.pptx",
+    input = "./report/html.Rmd",
+    output_file = "Ongoing.html",
     output_dir = "./Report",
     clean = TRUE,
     encoding = "utf8"
