e44624314e39320b62e0bd43d66609c0e588ba45
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-01-22 16:07:15 +0900

    TaxとWelfareのバランスを用いたスクリプトを追加 - #48

---
+++ script/R/#4TaxWelfare.r
@@ -0,0 +1,89 @@
+#4TaxWelfare
+
+## ---- library
+package_load <- function(pkg.name){
+  if (!require(pkg.name, character.only=TRUE)){
+    install.packages(pkg.name)
+  } else {
+    library(pkg.name, character.only = TRUE)
+  }
+}
+
+package_load("readstata13")  
+package_load("tidyverse")
+package_load("rlist")
+package_load("rlang")
+package_load("plm")
+package_load("lmtest")
+package_load("sandwich")
+package_load("lfe")
+package_load("Formula")
+
+## --- GGTemp
+my_theme <- theme_minimal() +
+  theme(
+    # setting: background
+    plot.background = element_rect(
+      #fill="#87CEEB50", 
+      color = "transparent"
+    ),
+    
+    # setting: plot
+    panel.border = element_rect(color = "white", fill = NA),
+    panel.background = element_rect(fill = "white"),   
+    panel.grid = element_line(color = "grey80"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    
+    # setting: text
+    plot.title = element_text(hjust=0.5,size=20),       
+    plot.caption = element_text(size=11),       
+    
+    # setting: axis
+    axis.text = element_text(color="black",size=13),    
+    axis.title = element_text(size=13),
+    axis.ticks.length = unit(0.25, "cm"),
+    axis.ticks.x = element_line(),
+    axis.ticks.y = element_line(),
+    axis.line = element_line(),
+    
+    # setting: legend
+    legend.title = element_text(size=12),               
+    legend.text = element_text(size=12),                
+    legend.key.size = unit(0.5,"cm"),
+    legend.background = element_rect(color = "black"), 
+    legend.position = "bottom"
+  )
+
+
+## ---- ReadData
+df <- read.dta13("data/shaped.dta")
+df <- data.frame(df) %>% 
+    mutate(
+        price = case_when(year < 2014 ~ 1 - mtr, TRUE ~ 1 -0.15),
+        log_price = log(price),
+        log_total_g = log(i_total_giving + 1),
+        log_pinc_all = log(lincome + 100000),
+        log_PPP_pubbdg = log(PPP_pubbdg + 1),
+        sqlog_PPP_pubbdg = log_PPP_pubbdg^2,
+        log_PPP_healthbdg = log(PPP_healthbdg + 1),
+        sqlog_PPP_healthbdg = log_PPP_healthbdg^2,
+        political_pref = factor(political_pref, level = c(3, 1, 2, 4, 5))
+    ) 
+df <- df %>% 
+  group_by(pid) %>% 
+  mutate(
+    lag1_price = dplyr::lag(price, n = 1, default = NA, order_by = year),
+    lag2_price = dplyr::lag(price, n = 2, default = NA, order_by = year),
+    lag3_price = dplyr::lag(price, n = 3, default = NA, order_by = year),
+    lag4_price = dplyr::lag(price, n = 4, default = NA, order_by = year)
+  ) %>% 
+  ungroup()
+df <- df %>% 
+  mutate(
+    lag1iv = log(price/lag1_price),
+    lag2iv = log(price/lag2_price),
+    lag3iv = log(price/lag3_price),
+    lag4iv = log(price/lag4_price),
+  )
