3e58d3c6069654b4db3cddac01f198003e234802
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-05-29 21:59:11 +0900

    efficient indexの異質性分析の追加 #63

--- script/R/03-tax_balance.r
+++ script/R/03-tax_balance.r
@@ -90,10 +90,15 @@ df <- read.dta13("data/shaped.dta") %>%
 
 balance_dt <- read.dta13("data/shape/balanceid.dta") %>% data.frame()
 
-df <- left_join(df, balance_dt, by = "pid")
-
+merge_df <- left_join(df, balance_dt, by = "pid") %>% 
+  mutate(balance3 = ntile(balanceid, 3)) %>%
+  mutate(
+    log_price_int2 = log_price * (balance3 == 2),
+    log_price_int3 = log_price * (balance3 == 3),
+  )
+  
 ## ---- HistogramTaxBalanceIndex
-df %>% 
+merge_df %>% 
   dplyr::select(pid, balanceid) %>% 
   distinct(.keep_all = TRUE) %>% 
   ggplot(aes(x = balanceid)) + 
@@ -102,7 +107,7 @@ df %>%
     my_theme
 
 ## ---- DensityTaxBalanceIndex
-df %>% 
+merge_df %>% 
   dplyr::select(pid, balanceid, ideal_balanceid) %>% 
   distinct(.keep_all = TRUE) %>% 
   ggplot(aes(x = balanceid)) +
@@ -113,3 +118,77 @@ df %>%
     labs(x = "Efficient index", linetype = "") +
     my_theme
 
+## ---- HeteroModel
+xlist_hetero <- list(
+  quote(log_price + log_price_int2 + log_price_int3 +  log_pinc_all + age + sqage + 
+    factor(year):factor(educ) + factor(year):factor(gender) + factor(year):factor(living_area))
+)
+fixef_hetero <- list(quote(year + pid))
+cluster_hetero <- list(quote(pid))
+
+#tabulation
+addline_hetero <- tibble(
+  vars = c("Individual FE", "Time FE", "Other controls"),
+  stat = "vars",
+  reg1 = "Y"
+)
+
+## ---- HeteroElasticity
+elast_hetero <- est_felm(
+  y = list(quote(log_total_g)), x = xlist_hetero,
+  fixef = fixef_hetero, cluster = cluster_hetero,
+  data = merge_df
+)
+
+i_elast_hetero <- est_felm(
+  y = list(quote(log_total_g)), x = xlist_hetero,
+  fixef = fixef_hetero, cluster = cluster_hetero,
+  data = subset(merge_df, i_ext_giving == 1)
+)
+
+e_elast_hetero <- est_felm(
+  y = list(quote(i_ext_giving)), x = xlist_hetero,
+  fixef = fixef_hetero, cluster = cluster_hetero,
+  data = merge_df
+)
+
+# tabulation
+tab.elast_hetero <- fullset_tab(
+  elast_hetero, 
+  keep_coef = c("log_price", "log_pinc_all"),
+  label_coef = list(
+    "log_price" = "ln(giving price)", 
+    "log_price_int2" = "ln(giving price) x 2Q efficient group", 
+    "log_price_int3" = "ln(giving price) x 3Q efficient group", 
+    "log_pinc_all" = "ln(annual taxable income)"), 
+  keep_stat = c("N", "R-squared"),
+  addline = addline_hetero
+)
+
+tab.i_elast_hetero <- fullset_tab(
+  i_elast_hetero, 
+  keep_coef = c("log_price", "log_pinc_all"),
+  label_coef = list(
+    "log_price" = "ln(giving price)", 
+    "log_price_int2" = "ln(giving price) x 2Q efficient group", 
+    "log_price_int3" = "ln(giving price) x 3Q efficient group", 
+    "log_pinc_all" = "ln(annual taxable income)"), 
+  keep_stat = c("N", "R-squared"),
+  addline = addline_hetero
+)
+
+tab.e_elast_hetero <- fullset_tab(
+  e_elast_hetero, 
+  keep_coef = c("log_price", "log_pinc_all"),
+  label_coef = list(
+    "log_price" = "ln(giving price)", 
+    "log_price_int2" = "ln(giving price) x 2Q efficient group", 
+    "log_price_int3" = "ln(giving price) x 3Q efficient group", 
+    "log_pinc_all" = "ln(annual taxable income)"), 
+  keep_stat = c("N", "R-squared"),
+  addline = addline_hetero
+)
+
+newtab.elast_hetero <- full_join(tab.elast_hetero$set, tab.e_elast_hetero$set, by = c("vars", "stat")) %>% 
+  full_join(tab.i_elast_hetero$set, by = c("vars", "stat")) %>% 
+  dplyr::rename(reg1 = reg1.x, reg2 = reg1.y, reg3 = reg1)
