56ddda8cf7e631b318e9daaede2e0cf4fc27f1b1
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-05-31 04:49:47 +0900

    slideとdraftをrender - #64

--- _render.r
+++ _render.r
@@ -1,16 +1,3 @@
-#' 
-#+ pandoc version (optional)
-Sys.setenv(RSTUDIO_PANDOC = "C:/Users/vge00/AppData/Local/Pandoc")
-#Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/Pandoc")
-
-#'
-#+ library and wd
-library(rmarkdown)
-library(bookdown)
-library(Statamarkdown)
-options(repo = "https://cran.rstudio.com/")
-options(bookdown.render.file_scope = FALSE)
-
 #'
 #+ Rmd File to Output
 
