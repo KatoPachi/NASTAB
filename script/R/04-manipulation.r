a61b4b842e6464b0637cc4170c70079d36de727f
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-06-26 14:53:03 +0900

    manipulationのRスクリプトの作成 - #72

---
+++ script/R/04-manipulation.r
@@ -0,0 +1,87 @@
+## ---- library
+library(xfun)
+xfun::pkg_attach2(c("readstata13", "tidyverse", "rlist"))
+xfun::pkg_attach2(c("plm", "lmtest", "sandwich", "lfe", "Formula"))
+source("script/R/00-analysis_functions.r")
+
+## --- GGTemp
+my_theme <- theme_minimal() +
+  theme(
+    # setting: background
+    plot.background = element_rect(
+      #fill="#87CEEB50", 
+      color = "transparent"
+    ),
+    
+    # setting: plot
+    panel.border = element_rect(color = "white", fill = NA),
+    panel.background = element_rect(fill = "white"),   
+    panel.grid = element_line(color = "grey80"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    
+    # setting: text
+    plot.title = element_text(hjust=0.5,size=20),       
+    plot.caption = element_text(size=11),       
+    
+    # setting: axis
+    axis.text = element_text(color="black",size=13),    
+    axis.title = element_text(size=13),
+    axis.ticks.length = unit(0.25, "cm"),
+    axis.ticks.x = element_line(),
+    axis.ticks.y = element_line(),
+    axis.line = element_line(),
+    
+    # setting: legend
+    legend.title = element_text(size=12),               
+    legend.text = element_text(size=12),                
+    legend.key.size = unit(0.5,"cm"),
+    #legend.background = element_rect(color = "black"), 
+    legend.position = "bottom"
+  )
+
+
+## ---- ReadData
+df <- read.dta13("data/shaped.dta") %>% 
+	data.frame() %>% 
+	mutate(
+		log_price = log(price),
+		log_lprice = log(lprice),
+		log_iv1price = log(iv1price),
+    log_iv2price = log(iv2price),
+    log_iv3price = log(iv3price),
+		log_total_g = log(i_total_giving + 1),
+		log_pinc_all = log(lincome + 100000),
+	) %>% 
+	group_by(pid) %>% 
+	mutate(
+		lag1_log_total_g = dplyr::lag(log_total_g, order_by = year),
+		lag2_log_total_g = dplyr::lag(log_total_g, order_by = year, n = 2),
+		lag3_log_total_g = dplyr::lag(log_total_g, order_by = year, n = 3),
+		lag1_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year),
+		lag2_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year, n = 2),
+		lag3_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year, n = 3),
+		lag1_age = dplyr::lag(age, order_by = year),
+		lag2_age = dplyr::lag(age, order_by = year, n = 2),
+		lag3_age = dplyr::lag(age, order_by = year, n = 3),
+		lag1_sqage = dplyr::lag(sqage, order_by = year),
+		lag2_sqage = dplyr::lag(sqage, order_by = year, n = 2),
+		lag3_sqage = dplyr::lag(sqage, order_by = year, n = 3)
+	) %>% 
+	ungroup() %>% 
+	mutate(
+		log_diff1g = log_total_g - lag1_log_total_g,
+		log_diff2g = log_total_g - lag2_log_total_g,
+		log_diff3g = log_total_g - lag3_log_total_g,
+		log_diff1I = log_pinc_all - lag1_log_pinc_all,
+		log_diff2I = log_pinc_all - lag2_log_pinc_all,
+		log_diff3I = log_pinc_all - lag3_log_pinc_all,
+		diff1_age = age - lag1_age,
+		diff2_age = age - lag2_age,
+		diff3_age = age - lag3_age,
+		diff1_sqage = sqage - lag1_sqage,
+		diff2_sqage = sqage - lag2_sqage,
+		diff3_sqage = sqage - lag3_sqage
+	) %>% 
+	filter(year >= 2012 & age >= 24) 
