3e3f0f75fca82f12bcd7c895d10d1aba419249e0
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-07-08 17:11:26 +0900

    censored_model.rをspinしてissue71.Rmdを作成 - #71

--- _render.r
+++ _render.r
@@ -1,15 +1,9 @@
-#' 
-#+ pandoc version (optional)
-Sys.setenv(RSTUDIO_PANDOC = "C:/Users/vge00/AppData/Local/Pandoc")
-#Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/Pandoc")
-
-#'
-#+ library and wd
-library(rmarkdown)
-library(bookdown)
-library(Statamarkdown)
-options(repo = "https://cran.rstudio.com/")
-options(bookdown.render.file_scope = FALSE)
+#' from R script to Rmd file
+#+
+knitr::spin(
+    "./script/R/censored_model.r",
+    knit = FALSE
+)
 
 #'
 #+ Rmd File to Output
