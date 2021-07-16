12e9ac04210fd4fc8c8bf36892ab6c1a6a79c46e
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-07-17 02:57:24 +0900

    elasticityのメインリザルトを修正 - #76

--- script/R/2-elasticity.r
+++ script/R/2-elasticity.r
@@ -1,174 +1,188 @@
 #1elasticity
 
-## ---- library
+# /* パッケージのロード
 library(xfun)
 xfun::pkg_attach2(c("readstata13", "tidyverse", "rlist"))
 xfun::pkg_attach2(c("plm", "lmtest", "sandwich", "lfe", "Formula"))
-source("script/R/00-analysis_functions.r")
+xfun::pkg_attach2("kableExtra")
 
-## --- GGTemp
-my_theme <- theme_minimal() +
-  theme(
-    # setting: background
-    plot.background = element_rect(
-      #fill="#87CEEB50", 
-      color = "transparent"
-    ),
-    
-    # setting: plot
-    panel.border = element_rect(color = "white", fill = NA),
-    panel.background = element_rect(fill = "white"),   
-    panel.grid = element_line(color = "grey80"),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    
-    # setting: text
-    plot.title = element_text(hjust=0.5,size=20),       
-    plot.caption = element_text(size=11),       
-    
-    # setting: axis
-    axis.text = element_text(color="black",size=13),    
-    axis.title = element_text(size=13),
-    axis.ticks.length = unit(0.25, "cm"),
-    axis.ticks.x = element_line(),
-    axis.ticks.y = element_line(),
-    axis.line = element_line(),
-    
-    # setting: legend
-    legend.title = element_text(size=12),               
-    legend.text = element_text(size=12),                
-    legend.key.size = unit(0.5,"cm"),
-    #legend.background = element_rect(color = "black"), 
-    legend.position = "bottom"
-  )
+lapply(Sys.glob(file.path("script/R/functions", "*.r")), source)
+# */
 
-
-## ---- ReadData
-df <- read.dta13("data/shaped.dta") %>% 
-	data.frame() %>% 
-	mutate(
-		log_price = log(price),
-		log_lprice = log(lprice),
-		log_iv1price = log(iv1price),
+# /* データの読み込み
+df <- read.dta13("data/shaped.dta") %>%
+  data.frame() %>%
+  mutate(
+    log_price = log(price),
+    log_lprice = log(lprice),
+    log_iv1price = log(iv1price),
     log_iv2price = log(iv2price),
     log_iv3price = log(iv3price),
-		log_total_g = log(i_total_giving + 1),
-		log_pinc_all = log(lincome + 100000),
-	) %>% 
-	group_by(pid) %>% 
-	mutate(
-		lag1_log_total_g = dplyr::lag(log_total_g, order_by = year),
-		lag2_log_total_g = dplyr::lag(log_total_g, order_by = year, n = 2),
-		lag3_log_total_g = dplyr::lag(log_total_g, order_by = year, n = 3),
-		lag1_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year),
-		lag2_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year, n = 2),
-		lag3_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year, n = 3),
-		lag1_age = dplyr::lag(age, order_by = year),
-		lag2_age = dplyr::lag(age, order_by = year, n = 2),
-		lag3_age = dplyr::lag(age, order_by = year, n = 3),
-		lag1_sqage = dplyr::lag(sqage, order_by = year),
-		lag2_sqage = dplyr::lag(sqage, order_by = year, n = 2),
-		lag3_sqage = dplyr::lag(sqage, order_by = year, n = 3)
-	) %>% 
-	ungroup() %>% 
-	mutate(
-		log_diff1g = log_total_g - lag1_log_total_g,
-		log_diff2g = log_total_g - lag2_log_total_g,
-		log_diff3g = log_total_g - lag3_log_total_g,
-		log_diff1I = log_pinc_all - lag1_log_pinc_all,
-		log_diff2I = log_pinc_all - lag2_log_pinc_all,
-		log_diff3I = log_pinc_all - lag3_log_pinc_all,
-		diff1_age = age - lag1_age,
-		diff2_age = age - lag2_age,
-		diff3_age = age - lag3_age,
-		diff1_sqage = sqage - lag1_sqage,
-		diff2_sqage = sqage - lag2_sqage,
-		diff3_sqage = sqage - lag3_sqage
-	) %>% 
-	filter(year >= 2012 & age >= 24) 
+    log_total_g = log(i_total_giving + 1),
+    log_pinc_all = log(lincome + 100000)
+  ) %>%
+  group_by(pid) %>%
+  mutate(
+    lag1_log_total_g = dplyr::lag(log_total_g, order_by = year),
+    lag2_log_total_g = dplyr::lag(log_total_g, order_by = year, n = 2),
+    lag3_log_total_g = dplyr::lag(log_total_g, order_by = year, n = 3),
+    lag1_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year),
+    lag2_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year, n = 2),
+    lag3_log_pinc_all = dplyr::lag(log_pinc_all, order_by = year, n = 3),
+    lag1_age = dplyr::lag(age, order_by = year),
+    lag2_age = dplyr::lag(age, order_by = year, n = 2),
+    lag3_age = dplyr::lag(age, order_by = year, n = 3),
+    lag1_sqage = dplyr::lag(sqage, order_by = year),
+    lag2_sqage = dplyr::lag(sqage, order_by = year, n = 2),
+    lag3_sqage = dplyr::lag(sqage, order_by = year, n = 3)
+  ) %>%
+  ungroup() %>%
+  mutate(
+    log_diff1g = log_total_g - lag1_log_total_g,
+    log_diff2g = log_total_g - lag2_log_total_g,
+    log_diff3g = log_total_g - lag3_log_total_g,
+    log_diff1I = log_pinc_all - lag1_log_pinc_all,
+    log_diff2I = log_pinc_all - lag2_log_pinc_all,
+    log_diff3I = log_pinc_all - lag3_log_pinc_all,
+    diff1_age = age - lag1_age,
+    diff2_age = age - lag2_age,
+    diff3_age = age - lag3_age,
+    diff1_sqage = sqage - lag1_sqage,
+    diff2_sqage = sqage - lag2_sqage,
+    diff3_sqage = sqage - lag3_sqage
+  ) %>%
+  filter(year >= 2012 & age >= 24)
+# */
 
-## ---- BaselineModel
-# regression model
+#' ## Price and Income Elasticity
+#'
+#+
 xlist <- list(
-  quote(log_price + log_pinc_all),
-  quote(log_price + log_pinc_all + age + sqage),
-  quote(log_price + log_pinc_all + age + sqage + factor(year):factor(educ)),
-  quote(log_price + log_pinc_all + age + sqage + factor(year):factor(educ) + factor(year):factor(gender)),
-  quote(log_price + log_pinc_all + age + sqage + factor(year):factor(educ) + factor(year):factor(gender) + 
-    factor(year):factor(living_area))
-)
-fixef <- list(quote(year + pid))
-cluster <- list(quote(pid))
-
-# wald test 
-wald <- list(
-  "Implied price elasticity" = "imp * log_price",
-  "Implied income elasticity" = "imp * log_pinc_all"
+  reg1 = ~ log_price + log_pinc_all,
+  reg2 = ~ log_price + log_pinc_all + age + sqage,
+  reg3 = ~ log_price + log_pinc_all + age + sqage + factor(year):factor(educ),
+  reg4 = ~ log_price + log_pinc_all + age + sqage + factor(year):factor(educ) +
+    factor(year):factor(gender),
+  reg5 = ~ log_price + log_pinc_all + age + sqage + factor(year):factor(educ) +
+    factor(year):factor(gender) + factor(year):factor(living_area)
 )
 
-# tabulation
-addline <- tribble(
+xlist_tab <- tribble(
   ~vars, ~stat, ~reg1, ~reg2, ~reg3, ~reg4, ~reg5,
   "Individual FE", "vars", "Y", "Y", "Y", "Y", "Y",
   "Time FE", "vars", "Y", "Y", "Y", "Y", "Y",
   "Age", "vars", "N", "Y", "Y", "Y", "Y",
-  "Year x Education", "vars", "N", "N", "Y", "Y", "Y", 
-  "Year x Gender", "vars", "N", "N", "N", "Y", "Y", 
-  "Year x Resident Area", "vars", "N", "N", "N", "N", "Y" 
+  "Year x Education", "vars", "N", "N", "Y", "Y", "Y",
+  "Year x Gender", "vars", "N", "N", "N", "Y", "Y",
+  "Year x Resident Area", "vars", "N", "N", "N", "N", "Y"
 )
 
-## ---- TotalElasticity
-# regressions
-elast <- est_felm(
-  y = list(quote(log_total_g)), x = xlist, 
-  fixef = fixef, cluster = cluster, 
-  data = df
-)
+#'
+#' We will estimate the elasticity without distinguishing them.
+#' We call this elasticity as overall elasticity.
+#' The price elasticity is rougly -1,
+#' which is statistically significant different from zero.
+#'
+#+
+overall <- xlist %>%
+  purrr::map(~ est_felm(
+    y = log_total_g ~ ., x = .,
+    fixef = ~ year + pid, cluster = ~pid,
+    data = df
+  ))
 
-# tabulation
-tab.elast <- fullset_tab(
-  elast$result, 
-  keep_coef = c("log_price", "log_pinc_all"),
-  label_coef = list("log_price" = "ln(giving price)", "log_pinc_all" = "ln(annual taxable income)"), 
-  keep_stat = c("N", "R-squared"), 
-  addlines = addline
-)
+overall %>%
+  felm_regtab(
+    keep_coef = c("log_price", "log_pinc_all"),
+    label_coef = list(
+      "log_price" = "ln(giving price)",
+      "log_pinc_all" = "ln(annual taxable income)"
+    )
+  ) %>%
+  regtab_addline(list(xlist_tab)) %>%
+  mutate(vars = if_else(stat == "se", "", vars)) %>%
+  dplyr::select(-stat) %>%
+  kable(
+    col.names = c("", sprintf("(%1d)", seq_len(length(overall)))),
+    align = "lccccc"
+  ) %>%
+  kable_styling()
 
-## ---- ExtElasticity
-# regressions
-e_elast <- est_felm(
-  y = list(quote(i_ext_giving)), x = xlist, 
-  fixef = fixef, cluster = cluster, 
-  wald_hypo = wald,
-  data = df
-)
+#'
+#' Compared to the overall elasticitiy,
+#' the intensive-margin elasticitiy of price and income are less elastic.
+#'
+#+
+intensive <- xlist %>%
+  purrr::map(~ est_felm(
+    y = log_total_g ~ ., x = .,
+    fixef = ~ year + pid, cluster = ~pid,
+    data = subset(df, i_ext_giving == 1)
+  ))
 
-# tabulation
-tab.e_elast <- fullset_tab(
-  e_elast$result, e_elast$test,
-  keep_coef = c("log_price", "log_pinc_all"),
-  label_coef = list("log_price" = "ln(giving price)", "log_pinc_all" = "ln(annual taxable income)"), 
-  keep_stat = c("N", "R-squared"), 
-  addlines = addline
-)
+intensive %>%
+  felm_regtab(
+    keep_coef = c("log_price", "log_pinc_all"),
+    label_coef = list(
+      "log_price" = "ln(giving price)",
+      "log_pinc_all" = "ln(annual taxable income)"
+    )
+  ) %>%
+  regtab_addline(list(xlist_tab)) %>%
+  mutate(vars = if_else(stat == "se", "", vars)) %>%
+  dplyr::select(-stat) %>%
+  kable(
+    col.names = c("", sprintf("(%1d)", seq_len(length(overall)))),
+    align = "lccccc"
+  ) %>%
+  kable_styling()
 
-## ---- IntElasticity
-# regressions
-i_elast <- est_felm(
-  y = list(quote(log_total_g)), x = xlist, 
-  fixef = fixef, cluster = cluster, 
-  data = subset(df, i_ext_giving == 1)
-)
+#'
+#' By results of overall elasticities and the intensive-margin elasticities,
+#' we expect that
+#' the extensive-margin price and income elasticity is more elastic
+#' than the overall elasticities.
+#'
+#' Although the implied price and income elasticity
+#' varies with covariates, results are in line with our expectation.
+#'
+#+
+extensive <- xlist %>%
+  purrr::map(~ est_felm(
+    y = i_ext_giving ~ ., x = .,
+    fixef = ~ year + pid, cluster = ~pid,
+    data = df
+  ))
 
-# tabulation
-tab.i_elast <- fullset_tab(
-  i_elast$result,
-  keep_coef = c("log_price", "log_pinc_all"),
-  label_coef = list("log_price" = "ln(giving price)", "log_pinc_all" = "ln(annual taxable income)"), 
-  keep_stat = c("N", "R-squared"), 
-  addlines = addline
-)
+ext_wald <- extensive %>%
+  purrr::map(~ felm_wald(
+    .,
+    hypo = list(
+      "Implied price elasticity" = "imp * log_price",
+      "Implied income elasticity" = "imp * log_pinc_all"
+    ),
+    args = list(imp = 1 / mean(.$response)),
+  )) %>%
+  reduce(full_join, by = c("vars", "stat")) %>%
+  setNames(c("vars", "stat", paste0("reg", seq_len(length(extensive)))))
+
+extensive %>%
+  felm_regtab(
+    keep_coef = c("log_price", "log_pinc_all"),
+    label_coef = list(
+      "log_price" = "ln(giving price)",
+      "log_pinc_all" = "ln(annual taxable income)"
+    )
+  ) %>%
+  regtab_addline(list(ext_wald, xlist_tab)) %>%
+  mutate(vars = if_else(stat == "se", "", vars)) %>%
+  dplyr::select(-stat) %>%
+  kable(
+    col.names = c("", sprintf("(%1d)", seq_len(length(overall)))),
+    align = "lccccc"
+  ) %>%
+  kable_styling()
+
 
 ## ---- LastElasticityModel
 # regeression model
