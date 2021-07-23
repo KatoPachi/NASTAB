debdd783ebe6ea88675ae5c4956fda003b6e8f62
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-07-24 03:59:54 +0900

    ext_benefit_tlの予測値を用いた推定を追加 - #76

--- script/R/6-self_selection.r
+++ script/R/6-self_selection.r
@@ -309,7 +309,7 @@ waldtest_ext <- list(ivest_xreport1_ext, ivest_xreport2_ext) %>%
   reduce(full_join, by = c("vars", "stat")) %>%
   setNames(c("vars", "stat", paste0("reg", seq_len(length(xlist) * 2)))) %>%
   bind_rows(
-    c(vars = "", stat = "", reg1 = "", reg2 = "", 
+    c(vars = "", stat = "", reg1 = "", reg2 = "",
       reg3 = "", reg4 = "", reg5 = "", reg6 = ""),
     .
   )
@@ -344,7 +344,7 @@ list(ivest_xreport1_ext, ivest_xreport2_ext) %>%
 
 #'
 #' intensive-margin elasticityのPanel IVの結果
-#' 
+#'
 #' - 個人固定効果を除くと価格の上昇が寄付の増加を促す（個人固定効果を入れていないのが原因？何らかの解釈が可能？）
 #' - 個人固定効果を入れると統計的に非有意となる（弱操作変数の影響とは考えにくい）
 #'
@@ -386,6 +386,220 @@ list(ivest_xreport1_int, ivest_xreport2_int) %>%
     label_coef = list("fit_int_price_benefit" = "Report x log(first price)")
   ) %>%
   regtab_addline(list(ivfstat_int_tab, xlist_duptab)) %>%
+  mutate(vars = if_else(stat == "se", "", vars)) %>%
+  dplyr::select(-stat) %>%
+  kable(
+    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
+    align = "lcccccc"
+  ) %>%
+  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
+  kable_styling()
+
+
+#'
+#' ## Panel IV: Endog var is tax report and IV is employed dummy.
+#'
+#' $\log g_{it}$を寄付の対数値、$\ln p_{it}$は寄付の相対価格（first price）とする。
+#' $D_{it}$はtax reportのダミー変数とし、first-stageの予測値を$\hat{D}_{it}$とする。
+#' Panel IVの推定において、個人固定効果$\alpha$と時間固定効果$\lambda$はコントロールする。
+#' 仮説検定では、個人レベルでクラスターした標準誤差を使用する。
+#'
+#' Second stage:
+#'
+#' $$
+#' \log g_{it}
+#' = \alpha_{2i} + \delta \hat{D}_{it} \log p_{it} + X_{it} \beta_2
+#' + \lambda_{2t} + \epsilon_{it}
+#' $$
+#'
+#' First stage:
+#'
+#' $$
+#' D_{it}
+#' = \alpha_{1i} + \gamma \text{Employed}_{it} + X_{it} \beta_1
+#' + \lambda_{1t} + \eta_{it}
+#' $$
+#'
+#' Panel first-stageのメッセージ
+#' - 自営業者より労働者がtax reportする確率は約50%高い
+#' - 個人固定効果をコントロールしても統計的に有意（F-値は最低でも80）
+#'
+#+
+est_report1 <- xlist %>%
+  purrr::map(~ fit_fixest(
+    y = ext_benefit_tl ~ ., x = .,
+    fixef = ~ year, cluster = ~ pid,
+    data = subset(df, year <= 2017)
+  ))
+
+est_report2 <- xlist %>%
+  purrr::map(~ fit_fixest(
+    y = ext_benefit_tl ~ ., x = .,
+    fixef = ~year + pid, cluster = ~pid,
+    data = subset(df, year <= 2017)
+  ))
+
+list(est_report1, est_report2) %>%
+  purrr::map(~ regtab_fixest(., keep_coef = "employee")) %>%
+  purrr::map(~ regtab_addline(., list(xlist_tab))) %>%
+  reduce(full_join, by = c("vars", "stat")) %>%
+  mutate(vars = if_else(stat == "se", "", vars)) %>%
+  dplyr::select(-stat) %>%
+  kable(
+    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
+    align = "lcccccc"
+  ) %>%
+  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
+  kable_styling()
+
+#'
+#' 上の表の(6)の予測値を$\hat{D}_{it}$とする。
+#+
+df$p_ext_benefit_tl <- predict(est_report2[[3]], df)
+
+#'
+#' Panel IVの推定結果のメッセージ
+#' - ITTとして推定した弾性値（約1%）より若干弾力的である
+#' - Tax reportをしていない人の価格は必ず$\log 1$であることを考えれば、納得できる結果
+#+
+ivest_report1 <- xlist %>%
+  purrr::map(~ fit_fixest(
+    y = log_total_g ~ .,
+    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
+    fixef = ~ year, cluster = ~ pid,
+    data = subset(df, year <= 2017)
+  ))
+
+ivest_report2 <- xlist %>%
+  purrr::map(~ fit_fixest(
+    y = log_total_g ~ .,
+    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
+    fixef = ~year + pid, cluster = ~ pid,
+    data = subset(df, year <= 2017)
+  ))
+
+list(ivest_report1, ivest_report2) %>%
+  flatten() %>%
+  regtab_fixest(
+    keep_coef = "log_price",
+    label_coef = list(
+      "log_price:p_ext_benefit_tl" = "Propensity of Report x log(first price)"
+    )
+  ) %>%
+  regtab_addline(list(xlist_duptab)) %>%
+  mutate(vars = if_else(stat == "se", "", vars)) %>%
+  dplyr::select(-stat) %>%
+  kable(
+    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
+    align = "lcccccc"
+  ) %>%
+  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
+  kable_styling()
+
+#'
+#' extensive-margin elasticityのPanel IVの結果
+#'
+#' - ITTとして想定したベースラインの結果（-1.4 ~ -1.2%）より、非弾力的になる（0.1%）
+#' - extensive-marginの弾力性はtax reportをしていない人に特に強い影響を与えるのか？
+#'
+#+
+ivest_report1_ext <- xlist %>%
+  purrr::map(~ fit_fixest(
+    y = i_ext_giving ~ .,
+    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
+    fixef = ~year, cluster = ~pid,
+    data = subset(df, year <= 2017)
+  ))
+
+ivest_report2_ext <- xlist %>%
+  purrr::map(~ fit_fixest(
+    y = i_ext_giving ~ .,
+    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
+    fixef = ~ year + pid, cluster = ~pid,
+    data = subset(df, year <= 2017)
+  ))
+
+waldtest_ext <- list(ivest_report1_ext, ivest_report2_ext) %>%
+  purrr::map_depth(2, ~ tibble(
+    vars = "Implied price elasticity",
+    coef = coef(.)["log_price:p_ext_benefit_tl"],
+    se = fixest::se(.)["log_price:p_ext_benefit_tl"],
+    invmu = 1 / mean(model.matrix(., type = "lhs")),
+    p = fixest::pvalue(.)["log_price:p_ext_benefit_tl"]
+  )) %>%
+  purrr::map_depth(2, function(x)
+    x %>%
+      mutate(
+        coef = case_when(
+          p <= .01 ~ sprintf("%1.3f***", coef / invmu),
+          p <= .05 ~ sprintf("%1.3f**", coef / invmu),
+          p <= .1 ~ sprintf("%1.3f*", coef / invmu),
+          TRUE ~ sprintf("%1.3f", coef / invmu)
+        ),
+        se = sprintf("(%1.3f)", se / invmu)
+      ) %>%
+      dplyr::select(-p, -invmu) %>%
+      pivot_longer(-vars, names_to = "stat", values_to = "val")
+  ) %>%
+  purrr::map(~ reduce(., full_join, by = c("vars", "stat"))) %>%
+  reduce(full_join, by = c("vars", "stat")) %>%
+  setNames(c("vars", "stat", paste0("reg", seq_len(length(xlist) * 2)))) %>%
+  bind_rows(
+    c(vars = "", stat = "", reg1 = "", reg2 = "",
+      reg3 = "", reg4 = "", reg5 = "", reg6 = ""),
+    .
+  )
+
+list(ivest_report1_ext, ivest_report2_ext) %>%
+  flatten() %>%
+  regtab_fixest(
+    keep_coef = "log_price",
+    label_coef = list(
+      "log_price:p_ext_benefit_tl" = "Propensity of Report x log(first price)"
+    )
+  ) %>%
+  regtab_addline(list(waldtest_ext, xlist_duptab)) %>%
+  mutate(vars = if_else(stat == "se", "", vars)) %>%
+  dplyr::select(-stat) %>%
+  kable(
+    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
+    align = "lcccccc"
+  ) %>%
+  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
+  kable_styling()
+
+#'
+#' intensive-margin elasticityのPanel IVの結果
+#'
+#' - 個人固定効果を加えると、ITTとして想定した弾性値（-1 ~ -0.5%）と似たような結果を取る
+#' - Intenstive-margin elasticityはtax reportをする人に特に影響を与えるのか？
+#'
+#+
+ivest_report1_int <- xlist %>%
+  purrr::map(~ fit_fixest(
+    y = log_total_g ~ .,
+    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
+    fixef = ~year, cluster = ~pid,
+    data = subset(df, i_ext_giving == 1 & year <= 2017)
+  ))
+
+ivest_report2_int <- xlist %>%
+  purrr::map(~ fit_fixest(
+    y = log_total_g ~ .,
+    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
+    fixef = ~ year + pid, cluster = ~pid,
+    data = subset(df, i_ext_giving == 1 & year <= 2017)
+  ))
+
+list(ivest_report1_int, ivest_report2_int) %>%
+  flatten() %>%
+  regtab_fixest(
+    keep_coef = "log_price",
+    label_coef = list(
+      "log_price:p_ext_benefit_tl" = "Propensity of Report x log(first price)"
+    )
+  ) %>%
+  regtab_addline(list(xlist_duptab)) %>%
   mutate(vars = if_else(stat == "se", "", vars)) %>%
   dplyr::select(-stat) %>%
   kable(
