30855ed1517abef2c5330747cd98881fba6dab04
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-07-23 20:47:38 +0900

    fit_関数に{fixest}を追加 - #77

--- script/R/functions/fit_fixest.r
+++ script/R/functions/fit_fixest.r
@@ -1,6 +1,6 @@
 #' ### Fixed effect modelの推定
 #'
-#' {lfe}の`felm`関数を用いて固定効果モデルやパネルIVモデルの推定を行う。
+#' {fixest}の`feols`関数を用いて固定効果モデルやパネルIVモデルの推定を行う。
 #' また、統計的推論ではクラスター標準誤差を用いることができる
 #'
 #' @param y アウトカム変数を含めたformulaオブジェクト (e.g. `y ~ .`)
@@ -8,39 +8,44 @@
 #' @param z 内生変数と操作変数を含めたformulaオブジェクト（e.g. `x3 ~ z`）
 #' @param fixef 固定効果を含めたformulaオブジェクト（e.g. `~ fix1 + fix2`）
 #' @param cluster クラスター標準誤差のクラスター変数を含めたformulaオブジェクト（e.g. `~ clust1`）
+#' @param se Character scalar: “standard”, “hetero”,
+#' “cluster”, “twoway”, “threeway” or “fourway”?
+#' By default if there are clusters in the estimation: se = "cluster",
+#' otherwise se = "standard"
 #' @param data 推定に用いるデータ
 #'
 #' @details `y`と`x`は必ず指定する必要がある。
-#' `z`、`fixed`、`cluster`は指定がない限り0で処理してformulaを作成する
 #'
 #' @return 推定結果のオブジェクト（`felm`クラス）を返す
 #+
-est_felm <- function(y, x, z = 0, fixef = 0, cluster = 0, data) {
+fit_fixest <- function(y, x, z = 0, fixef = 0, cluster = 0, data, se = NULL) {
 
   # make regression model
   yvar <- all.vars(y)[1]
   xeq <- as.character(x)[-1]
-  yxeq <- paste(yvar, "~", xeq)
 
-  fixeq <- zeq <- clusteq <- "0"
+  model <- paste(yvar, "~", xeq)
 
   if (fixef != 0) {
     fixeq <- all.vars(fixef) %>% paste(collapse = " + ")
+    model <- paste(model, "|", fixeq)
   }
 
   if (z != 0) {
-    zeq <- as.character(z)
-    zeq <- paste(zeq[2], "~", zeq[3]) %>% paste("(", ., ")")
+    zeq <- as.character(z); zeq <- paste(zeq[2], "~", zeq[3])
+    model <- paste(model, "|", zeq)
   }
 
-  if (cluster != 0) {
-    clusteq <- all.vars(cluster) %>% paste(collapse = " + ")
+  model <- as.formula(model)
+
+  # estimate fixed effect model using felm function provided by {fixest}
+  if (cluster == 0) {
+    if (is.null(se)) se <- "standard"
+    est_model <- fixest::feols(model, data = data, se = se)
+  } else {
+    if (is.null(se)) se <- "cluster"
+    est_model <- fixest::feols(model, data = data, cluster = cluster, se = se)
   }
-
-  model <- paste(yxeq, "|", fixeq, "|", zeq, "|", clusteq)
-
-  # estimate fixed effect model using felm function provided by {lfe}
-  est_model <- lfe::felm(as.formula(model), data = data)
 
   return(est_model)
 
