4ec654840a7b6d83484c42072f8814d43507bd0d
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-09-14 23:43:56 +0900

    所得階層別の寄付額の記述統計と2013年から2014年のデータを落とした推定を追加 - #96

--- script/R/7-issue96.r
+++ script/R/7-issue96.r
@@ -1,7 +1,7 @@
 # /*
 # knit
 knitr::spin(
-  "script/2-webexp_RCT.r",
+  "script/R/7-issue96.r",
   knit = TRUE
 )
 # */
@@ -12,9 +12,7 @@ knitr::opts_chunk$set(
   warning = FALSE,
   echo = FALSE,
   cache = FALSE,
-  include = TRUE,
-  fig.width = 10,
-  fig.height = 8
+  include = TRUE
 )
 
 library(here)
@@ -31,8 +29,9 @@ options(
 #+
 library(xfun)
 xfun::pkg_attach2(c(
-  "tidyverse", "rlang", "rlist", "lubridate",
+  "tidyverse", "rlang", "rlist",
   "lmtest", "sandwich",
+  "lfe", "Formula", "fixest",
   "kableExtra", "flextable", "officer", "modelsummary"
 ))
 
@@ -82,3 +81,208 @@ ggtemp <- function(flip = FALSE,
 
   return(my_theme)
 }
+
+#+
+df <- readr::read_csv(
+  "data/shaped2.csv",
+  col_types = cols(
+    ext_credit_giving = col_double(),
+    krw_credit_giving = col_double(),
+    trust_politician = col_double(),
+    political_pref = col_double(),
+    addtax = col_double(),
+    avg_welfare_tax = col_double(),
+    opt_welfare_tax = col_double(),
+    now_balance = col_double(),
+    ideal_balance = col_double()
+  )
+)
+
+#'
+#' # Issue96に関するレポート
+#'
+#' [tracking issue here](https://github.com/KatoPachi/NASTAB/issues/96)
+#'
+#' ## 異時点間の代替性の検証
+#'
+#' - 問題の所在：2014年の寄付税制のアナウンスが2013年にあったので、これによる異時点間の代替性が生じているかもしれない
+#'   - Anticipation effect
+#'   - 所得ラグの推定は所得税の累進性に対応したものなので、この効果に対する明確な対処となっていない
+#' - 対応１：所得階層別の寄付額の記述統計の確認する
+#'   - アナウンスによる異時点間の代替性が視覚的に生じているかどうか
+#'   - 平行トレンドが満たされていれば、anticipation effectは特に問題なし
+#' - 以下の図は所得階層を3区分にして、各所得階層の各年の寄付額の対数値の平均をプロット
+#'   - income < 1200：2014年改革で寄付の相対価格が下落するグループ
+#'   - income b/w 1200 and 4600：2014年改革で寄付の相対価格が変化しないグループ
+#'   - income > 4600：2014年改革で寄付の相対価格が増加するグループ
+#'   - **視覚的に、異時点間の代替性が生じているとは思えないし、平行トレンドも満たされているようにも見える**
+#'
+#+ SummaryOutcomebyIncome1
+df %>%
+  mutate(group = case_when(
+    credit_loss == 1 ~ 3,
+    credit_neutral == 1 ~ 2,
+    credit_benefit == 1 ~ 1
+  )) %>%
+  dplyr::filter(!is.na(group)) %>%
+  group_by(year, group) %>%
+  summarize(mu = mean(log_total_g, na.rm = TRUE)) %>%
+  mutate(group = factor(
+    group, labels = c("< 1200", "[1200, 4600]", "> 4600")
+  )) %>%
+  ggplot(aes(x = year, y = mu, group = group)) +
+    geom_point(aes(color = group)) +
+    geom_line(aes(color = group)) +
+    geom_vline(aes(xintercept = 2013.5), linetype = 3) +
+    scale_x_continuous(breaks = seq(2012, 2018, 1)) +
+    labs(
+      x = "Year", y = "Mean donations (logged value)",
+      color = "Income group (unit:10,000KRW)"
+    ) +
+    ggtemp()
+
+#'
+#' - 2013年の寄付対数値の平均が1となるようにしても、平行トレンドが満たされていて、異時点間の代替性が生じていると思われる
+#'
+#+ SummaryOutcomebyIncome2
+df %>%
+  mutate(group = case_when(
+    credit_loss == 1 ~ 3,
+    credit_neutral == 1 ~ 2,
+    credit_benefit == 1 ~ 1
+  )) %>%
+  dplyr::filter(!is.na(group)) %>%
+  group_by(year, group) %>%
+  summarize(mu = mean(log_total_g, na.rm = TRUE)) %>%
+  tidyr::pivot_wider(names_from = "year", values_from = "mu") %>%
+  mutate(base = `2013`) %>%
+  dplyr::select(group, base, everything()) %>%
+  tidyr::pivot_longer(- (group:base), values_to = "mu", names_to = "year") %>%
+  mutate(mu = mu / base, year = as.numeric(year)) %>%
+  mutate(group = factor(
+    group,
+    labels = c("< 1200", "[1200, 4600]", "> 4600")
+  )) %>%
+  ggplot(aes(x = year, y = mu, group = group)) +
+  geom_point(aes(color = group)) +
+  geom_line(aes(color = group)) +
+  geom_vline(aes(xintercept = 2013.5), linetype = 3) +
+  scale_x_continuous(breaks = seq(2012, 2018, 1)) +
+  labs(
+    x = "Year",
+    y = "The ratio of mean donations (logged value)",
+    color = "Income group (unit:10,000KRW)",
+    caption = paste(
+      "The ratio is calculated by",
+      "(mean of logged donation in year t) / (mean of logged donation in 2013)."
+    )
+  ) +
+  ggtemp()
+
+#'
+#' - 対策２：異時点間の代替性によって影響をうける制度改革の直前と直後のデータを落として、推定する
+#'   - 以下の表は2013年と2014年のデータを落として、First priceの弾力性を推定した結果
+#'   - 比較のために、フルサンプルの結果も併せて示す
+#'   - データを落としても、結果に大きな変化はない
+#'   - **異時点間の代替性があったとしても、結果は大きく変化しない**
+#+
+fixest::setFixest_fml(
+  ..ctrl = ~ log_pinc_all + age + sqage + factor(year):factor(educ) +
+    factor(year):factor(gender) + factor(year):factor(living_area) | year + pid
+)
+
+xlist_tab <- tribble(
+  ~term, ~Overall, ~Intensive, ~Extensive,
+  "Age (squared age)", "X", "X", "X",
+  "Year x Education", "X", "X", "X",
+  "Year x Gender", "X", "X", "X",
+  "Year x Resident Area", "X", "X", "X"
+)
+
+df %>%
+  {
+    list(
+      "Overall" = list(
+        eq = fixest::xpd(log_total_g ~ log_price + ..ctrl),
+        data = .
+      ),
+      "Intensive" = list(
+        eq = fixest::xpd(log_total_g ~ log_price + ..ctrl),
+        data = subset(., i_ext_giving == 1)
+      ),
+      "Extensive" = list(
+        eq = fixest::xpd(i_ext_giving ~ log_price + ..ctrl),
+        data = .
+      )
+    ) 
+  } %>%
+  purrr::map(~ fixest::feols(
+    .$eq, data = .$data,
+    cluster = ~ pid, se = "cluster"
+  )) %>%
+  modelsummary(
+    title = "First price elasticity: Full sample",
+    coef_omit = "factor|age",
+    coef_rename = c(
+      "log_price" = "log(first giving price)",
+      "log_pinc_all" = "log(annual taxable income)"
+    ),
+    stars = c("*" = .1, "**" = .05, "***" = .01),
+    gof_omit = "^(?!R2 Adj.|R2 Within|FE|N)",
+    add_rows = xlist_tab
+  ) %>%
+  kableExtra::footnote(
+    general_title = "",
+    general = paste(
+      "Note: * p < 0.1, ** p < 0.05, *** p < 0.01.",
+      "Starand errors are culustered at individual level."
+    ),
+    threeparttable = TRUE,
+    escape = FALSE
+  )
+
+
+#'
+#+
+df %>%
+  dplyr::filter(year < 2013 | year > 2014) %>%
+  {
+    list(
+      "Overall" = list(
+        eq = fixest::xpd(log_total_g ~ log_price + ..ctrl),
+        data = .
+      ),
+      "Intensive" = list(
+        eq = fixest::xpd(log_total_g ~ log_price + ..ctrl),
+        data = subset(., i_ext_giving == 1)
+      ),
+      "Extensive" = list(
+        eq = fixest::xpd(i_ext_giving ~ log_price + ..ctrl),
+        data = .
+      )
+    ) 
+  } %>%
+  purrr::map(~ fixest::feols(
+    .$eq, data = .$data,
+    cluster = ~ pid, se = "cluster"
+  )) %>%
+  modelsummary(
+    title = "First price elasticity: Exclude data with year 2013 and 2014",
+    coef_omit = "factor|age",
+    coef_rename = c(
+      "log_price" = "log(first giving price)",
+      "log_pinc_all" = "log(annual taxable income)"
+    ),
+    stars = c("*" = .1, "**" = .05, "***" = .01),
+    gof_omit = "^(?!R2 Adj.|R2 Within|FE|N)",
+    add_rows = xlist_tab
+  ) %>%
+  kableExtra::footnote(
+    general_title = "",
+    general = paste(
+      "Note: * p < 0.1, ** p < 0.05, *** p < 0.01.",
+      "Starand errors are culustered at individual level."
+    ),
+    threeparttable = TRUE,
+    escape = FALSE
+  )
