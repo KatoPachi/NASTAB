2e4b49d1da7a87085e601e51d66ef64f3e4a0035
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2021-10-25 20:38:22 +0900

    1-summary.rを修正 - #96

--- script/R/functions/ggtemp.r
+++ script/R/functions/ggtemp.r
@@ -2,26 +2,33 @@
 #'
 #' ggplot2のテンプレート関数
 #'
-#' @param flip `coord_flip()`をするかどうかのT/F変数。TRUEなら描画する軸を逆転させる
-#'
-#' @return `gg`と`theme`クラスのオブジェクトを返す
 #+
-ggtemp <- function(flip = FALSE) {
-
-  my_theme <- theme_minimal() +
+ggtemp <- function(flip = FALSE,
+                   family = NULL,
+                   size = list(
+                     title = 13,
+                     text = 9,
+                     caption = 11
+                   ),
+                   legend_key_size = 1) {
+  my_theme <- theme_minimal(base_family = family) +
     theme(
       panel.grid.major.x = element_blank(),
       panel.grid.minor.x = element_blank(),
       panel.grid.minor.y = element_blank(),
-      axis.text = element_text(color = "black", size = 13),
-      axis.title = element_text(size = 13),
+      axis.text = element_text(
+        color = "black", size = size$text, family = family
+      ),
+      axis.title = element_text(size = size$title, family = family),
       axis.ticks.length = unit(0.25, "cm"),
       axis.ticks.x = element_line(),
       axis.ticks.y = element_line(),
       axis.line = element_line(),
-      legend.text = element_text(size = 13),
-      legend.key.size = unit(1, "cm"),
-      legend.position = "bottom"
+      legend.text = element_text(size = size$text, family = family),
+      legend.key.size = unit(legend_key_size, "cm"),
+      legend.title = ggplot2::element_text(size = size$title),
+      legend.position = "bottom",
+      plot.caption = ggplot2::element_text(size = size$caption)
     )
 
   if (flip) {
@@ -33,5 +40,4 @@ ggtemp <- function(flip = FALSE) {
   }
 
   return(my_theme)
-
 }
