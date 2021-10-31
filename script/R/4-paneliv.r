f66959229cededfb6f9a267b1e52e38f50bd335b
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-10-31 23:33:07 +0900

    population modelのベースライン推定を追加 - #96

--- script/R/4-paneliv.r
+++ script/R/4-paneliv.r
@@ -1,16 +1,49 @@
-#' Issue #76: ITTの問題を克服するために、PSM-DIDのストラテジーを利用した推定
+#' ---
+#' title: |
+#'   Preview: Recovering Population Elasticities
+#' author: Hiroki Kato
+#' output:
+#'   html_document:
+#'     toc: true
+#'     toc_float: true
+#' params:
+#'   preview: true
+#' ---
 #'
-#' パッケージのロード
-#+
+#+ include = FALSE, eval = params$preview
+knitr::opts_chunk$set(
+  message = FALSE,
+  warning = FALSE,
+  echo = FALSE,
+  cache = FALSE,
+  include = TRUE,
+  fig.width = 10
+)
+
+library(here)
+knitr::opts_knit$set(
+  root.dir = here::here()
+)
+
+options(
+  knitr.kable.NA = " ",
+  knitr.table.format = "html",
+  modelsummary_stars_note = FALSE
+)
+
+
+#'
+#+ include = FALSE, eval = params$preview
 library(xfun)
-xfun::pkg_attach2(c("tidyverse", "rlist"))
-xfun::pkg_attach2(c("lmtest", "sandwich", "lfe", "Formula", "fixest"))
-xfun::pkg_attach2("kableExtra")
+xfun::pkg_attach2(c(
+  "tidyverse", "rlist", "modelsummary", "kableExtra",
+  "estimatr", "fixest"
+))
 
 lapply(Sys.glob(file.path("script/R/functions", "*.r")), source)
 
-#' データのロード
-#+
+#'
+#+ include = FALSE, eval = params$preview
 df <- readr::read_csv(
   "data/shaped2.csv",
   col_types = cols(
@@ -26,200 +59,48 @@ df <- readr::read_csv(
   )
 )
 
-#'
-#' ## 寄付の税申告ファクトの整理
-#'
-#' 勤労所得もしくは総合所得において寄付の控除申告をしたかどうかをクロス集計で確認する。
-#' 勤労所得もしくは控除申告がN/Aの扱いは以下の通り
-#'
-#' - 勤労所得もしくは総合所得が0ならば、控除申告はなしとみなす
-#' - それ以外の場合、欠損値としてみなす（「回答したくない」と「申告していない」を区別できないから）
-#'
-#' **メッセージ：税制改革以降のtax reportは過半数を下回る。**
-#+
-df %>%
-  dplyr::filter(year <= 2017) %>%
-  group_by(year) %>%
-  summarize_at(
-    vars(ext_benefit_t, ext_benefit_tl),
-    list(
-      Yes = ~sum((. == 1), na.rm = TRUE),
-      No = ~sum((. == 0), na.rm = TRUE)
-    )
-  ) %>%
-  kable(
-    col.names = c(
-      "Year", "Labor inc", "Labor and Total inc",
-      "Labor inc", "Labor and Total inc"
-    ),
-    align = "lcccc"
-  ) %>%
-  add_header_above(c("", "Tax report" = 2, "No tax report" = 2)) %>%
-  kable_styling()
-
-#'
-#' 以下の就業形態に関する変数を用いる
-#'
-#' - `p_aa005`：雇用形態（1=常用職2=臨時職日雇い3=自営業4=無給家族従事者）
-#' - `paa008`：産業
-#'     - 農業
-#'     - 林業や漁業
-#'     - 鉱業
-#'     - 製造業
-#'     - 電気、ガスおよび水道事業
-#'     - 下水∙、廃棄物処理、原料再生および環境ボクウォンオプ
-#'     - 建設業
-#'     - 卸売や小売業
-#'     - 運輸業
-#'     - 宿泊と飲食店業
-#'     - 出版、映像、放送通信や情報サービス業
-#'     - 金融および保険業
-#'     - 不動産業及び賃貸業
-#'     - 専門科学および技術サービス業
-#'     - 事業施設管理および事業支援サービス業
-#'     - 公共行政、国防や、社会保障、行政
-#'     - 教育サービス業
-#'     - 保険業や社会福祉サービス業
-#'     - 芸術、スポーツや余暇関連サービス業
-#'     - 協会や団体、修理および個人サービス業
-#'     - 世帯内雇用活動と違って分類されていない者が消費、生産活動
-#'     - 国際および外国機関
-#'
-#' `p_aa005`で常用職かどうかのダミー変数を作成する。`paa008`は共変量としてコントロールする。
-#'
-#' employeeかどうかで控除申請に差があるかどうかを記述統計で確認する。
-#' 初めに労働所得で控除申請している比率を示す。
-#'
-#' メッセージ：労働者の方がtax reportする比率が自営業者よりも高い
-#'
-#+
-df %>%
-  dplyr::filter(year <= 2017) %>%
-  dplyr::filter(!is.na(employee)) %>%
-  group_by(year, employee) %>%
-  summarize_at(vars(ext_benefit_t), list(~ mean(., na.rm = TRUE))) %>%
-  mutate(employee = factor(employee)) %>%
-  ggplot(aes(x = year, y = ext_benefit_t, group = employee)) +
-  geom_point(aes(shape = employee), color = "black", size = 3) +
-  geom_line(aes(linetype = employee), size = 1) +
-  geom_vline(aes(xintercept = 2013.5)) +
-  scale_x_continuous(breaks = seq(2012, 2018, 1)) +
-  labs(y = "share of tax report") +
-  ggtemp()
-
-#'
-#' 次に、総合所得もしくは労働所得の項目で控除申請しているかどうかの比率を示す
-#'
-#' この図のメッセージ
-#' - 労働者の申請比率はほぼ100%に近くなる = 総合所得で税控除を申請している
-#' - 自営業者の申請比率は変化していない
-#'
-#+
-df %>%
-  dplyr::filter(year <= 2017) %>%
-  dplyr::filter(!is.na(employee)) %>%
-  group_by(year, employee) %>%
-  summarize_at(vars(ext_benefit_tl), list(~ mean(., na.rm = TRUE))) %>%
-  mutate(employee = factor(employee)) %>%
-  ggplot(aes(x = year, y = ext_benefit_tl, group = employee)) +
-  geom_point(aes(shape = employee), color = "black", size = 3) +
-  geom_line(aes(linetype = employee), size = 1) +
-  geom_vline(aes(xintercept = 2013.5)) +
-  scale_x_continuous(breaks = seq(2012, 2018, 1)) +
-  labs(y = "share of tax report") +
-  ggtemp()
-
-
-#'
-#' ## Simple Panel Data Model
-#'
-#' Panel IVを使う意義として、スタンダードな固定効果モデルの推定結果を示す。
-#' 推定モデルは
-#'
-#' $$
-#' \log g_{it}
-#' = \alpha_{2i} + \delta D_{it} \log p_{it} + X_{it} \beta_2
-#' + \lambda_{2t} + \epsilon_{it}
-#' $$
-#'
-#' 統計的推論では個人レベルのクラスター標準誤差を使用する。
-#'
-#' Simple panel data modelのメッセージ
-#' - 全体の弾力性とintensive-margin elasticityはITTの時よりも過大推定
-#' - Extensive-margin elasticityはITTの弾性値の時よりも過小推定
-#' - Tax reportが絡むと弾性値が変わるので、「reportした人とそうでない人の異質性」か「観察不可能な要因との交絡」が考えられる
-#'
-#+
-simplemodel <- list(
-  overall = list(y = log_total_g ~ ., data = subset(df, year <= 2017)),
-  ext = list(y = i_ext_giving ~ ., data = subset(df, year <= 2017)),
-  ext = list(
-    y = log_total_g ~ .,
-    data = subset(df, i_ext_giving == 1 & year <= 2017)
-  )
+fixest::setFixest_fml(
+  ..first1 = ~ log_pinc_all | year + panelid,
+  ..first2 = ~ log_pinc_all + sqage | year + panelid,
+  ..first3 = ~ log_pinc_all + sqage | year + panelid + area,
+  ..first4 = ~ log_pinc_all + sqage | year + panelid + area + industry
 )
 
-est_simplemodel <- simplemodel %>%
-  purrr::map(~ fit_fixest(
-    y = .$y,
-    x = ~ log_price:ext_benefit_tl + log_pinc_all + age + sqage +
-      factor(year):factor(educ) + factor(year):factor(gender) +
-      factor(year):factor(indust),
-    fixef = ~year + pid, cluster = ~pid, data = .$data
+
+#'
+#' ## Recovering Population Elasticities
+#'
+#'
+#+
+popmod <- list(
+  "(1)" = fixest::xpd(log_total_g ~ log_price:ext_benefit_tl + ..first1),
+  "(2)" = fixest::xpd(log_total_g ~ log_price:ext_benefit_tl + ..first2),
+  "(3)" = fixest::xpd(log_total_g ~ log_price:ext_benefit_tl + ..first3),
+  "(4)" = fixest::xpd(log_total_g ~ log_price:ext_benefit_tl + ..first4)
+)
+
+popmod %>%
+  purrr::map(~ fixest::feols(
+    .,
+    data = subset(df, i_ext_giving == 1),
+    cluster = ~ panelid, se = "cluster"
   )) %>%
-  regtab_fixest(
-    keep_coef = "log_price:ext_benefit_tl",
-    label_coef = list("log_price:ext_benefit_tl" = "Report x log(first price)")
+  modelsummary(
+    title = "Population First-Price Elasiticities",
+    coef_map = c(
+      "log_price:ext_benefit_tl" =
+        "log(first giving price) x Applying tax relief",
+      "log_pinc_all" =
+        "log(annual taxable income)"
+    ),
+    gof_omit = "^(?!R2 Adj.|FE|N|Std.Errors)",
+    stars = c("*" = .1, "**" = .05, "***" = .01),
+    add_rows = tribble(
+      ~term, ~"(1)", ~"(2)", ~"(3)", ~"(4)",
+      "Square age", "", "X", "X", "X"
+    )
   )
 
-simplemodel_tab <- tribble(
-  ~vars, ~stat, ~reg1, ~reg2, ~reg3,
-  "Individual and time FE", "vars", "Y", "Y", "Y",
-  "log(income)", "vars", "Y", "Y", "Y",
-  "Age", "vars", "Y", "Y", "Y",
-  "Year x Education", "vars", "Y", "Y", "Y",
-  "Year x Gender", "vars", "Y", "Y", "Y",
-  "Year x Resident Area", "vars", "Y", "Y", "Y",
-  "Year x Dummy of industry", "vars", "Y", "Y", "Y"
-)
-
-est_simplemodel %>%
-  regtab_addline(list(simplemodel_tab)) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", "Overall", "Extensive", "Intensive"),
-    align = "lccc"
-  ) %>%
-  kable_styling()
-
-#'
-#' ## Panel IV: Endog var is tax report x giving price and IV is employed dummy.
-#'
-#' $\log g_{it}$を寄付の対数値、$\ln p_{it}$は寄付の相対価格（first price）とする。
-#' $D_{it}$はtax reportのダミー変数とする。
-#' Panel IVの推定において、個人固定効果$\alpha$と時間固定効果$\lambda$はコントロールする。
-#' 仮説検定では、個人レベルでクラスターした標準誤差を使用する。
-#'
-#' Second stage:
-#'
-#' $$
-#' \log g_{it}
-#' = \alpha_{2i} + \delta D_{it} \log p_{it} + X_{it} \beta_2
-#' + \lambda_{2t} + \epsilon_{it}
-#' $$
-#'
-#' First stage:
-#'
-#' $$
-#' D_{it} \log p_{it}
-#' = \alpha_{1i} + \gamma \text{Employed}_{it} + X_{it} \beta_1
-#' + \lambda_{1t} + \eta_{it}
-#' $$
-#'
-#' Panel first-stageのメッセージ
-#' - 自営業者より労働者はtax reportをして寄付の相対価格を下げている
-#' - 個人固定効果をコントロールしても統計的に有意（F-値は最低でも70）
 #'
 #+
 xlist <- list(
