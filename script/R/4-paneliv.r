f0a2b083e1a38325a602cfab86184aac8c7fadbe
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-11-01 02:08:42 +0900

    2SLSとCF estimatorを追加 - #96
    細かいところは一応無視

--- script/R/4-paneliv.r
+++ script/R/4-paneliv.r
@@ -82,7 +82,7 @@ popmod <- list(
 popmod %>%
   purrr::map(~ fixest::feols(
     .,
-    data = subset(df, i_ext_giving == 1),
+    data = subset(df, i_ext_giving == 1 & year <= 2017),
     cluster = ~ panelid, se = "cluster"
   )) %>%
   modelsummary(
@@ -103,470 +103,142 @@ popmod %>%
 
 #'
 #+
-xlist <- list(
-  ~ employee,
-  ~ employee + log_pinc_all + age + sqage +
-    factor(year):factor(educ) + factor(year):factor(gender),
-  ~ employee + log_pinc_all + age + sqage +
-    factor(year):factor(educ) + factor(year):factor(gender) +
-    factor(year):factor(indust)
+fixest::setFixest_fml(
+  ..stage1 = ~ log_pinc_all + sqage + factor(area) + factor(industry)
 )
 
-xlist_tab <- tribble(
-  ~vars, ~stat, ~reg1, ~reg2, ~reg3,
-  "Time FE", "vars", "Y", "Y", "Y",
-  "log(income)", "vars", "N", "Y", "Y",
-  "Age", "vars", "N", "Y", "Y",
-  "Year x Education", "vars", "N", "Y", "Y",
-  "Year x Gender", "vars", "N", "Y", "Y",
-  "Year x Resident Area", "vars", "N", "Y", "Y",
-  "Year x Dummy of industry", "vars", "N", "N", "Y"
+poolstage1 <- fixest::feglm(
+  fixest::xpd(ext_benefit_tl ~ employee + log_price + ..stage1),
+  family = binomial(link = "probit"),
+  data = subset(df, year <= 2017)
 )
 
-est_xreport1 <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = int_price_benefit ~ ., x = .,
-    fixef = ~ year, cluster = ~ pid,
-    data = subset(df, year <= 2017)
+sepstage1 <- df %>%
+  dplyr::filter(year <= 2017) %>%
+  group_by(year) %>%
+  do(sepmod = fixest::feglm(
+    fixest::xpd(ext_benefit_tl ~ employee + log_price + ..stage1),
+    family = binomial(link = "probit"),
+    data = .
   ))
 
-est_xreport2 <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = int_price_benefit ~ ., x = .,
-    fixef = ~year + pid, cluster = ~pid,
-    data = subset(df, year <= 2017)
-  ))
-
-list(est_xreport1, est_xreport2) %>%
-  purrr::map(~ regtab_fixest(., keep_coef = "employee")) %>%
-  purrr::map(~ regtab_addline(., list(xlist_tab))) %>%
-  reduce(full_join, by = c("vars", "stat")) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
-    align = "lcccccc"
-  ) %>%
-  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
-  kable_styling()
-
-#'
-#' Panel IVの推定結果のメッセージ
-#' - 弾力性は10%。1%の相対価格の上昇が寄付額を10%程度下げる
-#' - ITTで考えていたよりもかなり弾力的な結果となる
-#+
-ivest_xreport1 <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = log_total_g ~ .,
-    x = update(., ~ . - employee),
-    z = int_price_benefit ~ employee,
-    fixef = ~ year, cluster = ~ pid,
-    data = subset(df, year <= 2017)
-  ))
-
-ivest_xreport2 <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = log_total_g ~ .,
-    x = update(., ~ . - employee),
-    z = int_price_benefit ~ employee,
-    fixef = ~year + pid, cluster = ~ pid,
-    data = subset(df, year <= 2017)
-  ))
-
-ivfstat <- list(ivest_xreport1, ivest_xreport2) %>%
-  purrr::map_depth(2, ~ fitstat(., ~ ivwald)$ivwald1$stat) %>%
-  purrr::map(~ as_vector(.)) %>%
-  as_vector() %>%
-  matrix(nrow = 1) %>%
-  data.frame() %>%
-  setNames(paste0("reg", seq_len(length(xlist) * 2)))
-
-ivfstat_tab <- ivfstat %>%
-  mutate_all(list(~ sprintf("%1.3f", .))) %>%
-  mutate(vars = "Fstat of IV", stat = "vars")
-
-xlist_duptab <- xlist_tab %>%
-  full_join(xlist_tab, by = c("vars", "stat")) %>%
-  setNames(c("vars", "stat", paste0("reg", seq_len(length(xlist) * 2))))
-
-list(ivest_xreport1, ivest_xreport2) %>%
-  flatten() %>%
-  regtab_fixest(
-    keep_coef = "int_price_benefit",
-    label_coef = list("fit_int_price_benefit" = "Report x log(first price)")
-  ) %>%
-  regtab_addline(list(ivfstat_tab, xlist_duptab)) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
-    align = "lcccccc"
-  ) %>%
-  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
-  kable_styling()
-
-#'
-#' extensive-margin elasticityのPanel IVの結果
-#'
-#' - 価格1%上昇によって、寄付する確率を1%下げる
-#' - この結果はITTで考えた推定結果と非常に似ている。
-#'
-#+
-ivest_xreport1_ext <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = i_ext_giving ~ .,
-    x = update(., ~ . - employee),
-    z = int_price_benefit ~ employee,
-    fixef = ~year, cluster = ~pid,
-    data = subset(df, year <= 2017)
-  ))
-
-ivest_xreport2_ext <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = i_ext_giving ~ .,
-    x = update(., ~ . - employee),
-    z = int_price_benefit ~ employee,
-    fixef = ~ year + pid, cluster = ~pid,
-    data = subset(df, year <= 2017)
-  ))
-
-waldtest_ext <- list(ivest_xreport1_ext, ivest_xreport2_ext) %>%
-  purrr::map_depth(2, ~ tibble(
-    vars = "Implied price elasticity",
-    coef = coef(.)["fit_int_price_benefit"],
-    se = fixest::se(.)["fit_int_price_benefit"],
-    invmu = 1 / mean(model.matrix(., type = "lhs")),
-    p = fixest::pvalue(.)["fit_int_price_benefit"]
+estdf <- df %>%
+  dplyr::filter(year <= 2017) %>%
+  mutate(poolmod = list(poolstage1)) %>%
+  left_join(sepstage1, by = "year") %>%
+  group_by(year) %>%
+  do(modelr::spread_predictions(
+    ., first(.$poolmod), first(.$sepmod),
+    type = "link"
   )) %>%
-  purrr::map_depth(2, function(x)
-    x %>%
-      mutate(
-        coef = case_when(
-          p <= .01 ~ sprintf("%1.3f***", coef / invmu),
-          p <= .05 ~ sprintf("%1.3f**", coef / invmu),
-          p <= .1 ~ sprintf("%1.3f*", coef / invmu),
-          TRUE ~ sprintf("%1.3f", coef / invmu)
-        ),
-        se = sprintf("(%1.3f)", se / invmu)
-      ) %>%
-      dplyr::select(-p, -invmu) %>%
-      pivot_longer(-vars, names_to = "stat", values_to = "val")
+  rename(lpred_pool = "first(.$poolmod)", lpred_sep = "first(.$sepmod)") %>%
+  mutate(
+    # propensity score
+    psc_pool = pnorm(lpred_pool), psc_sep = pnorm(lpred_sep),
+
+    # inverse mills ratio
+    imr_pool = dnorm(lpred_pool) / pnorm(lpred_pool),
+    imr_sep = dnorm(lpred_sep) / pnorm(lpred_sep),
+
+    # reverse of inverse mills ratio
+    rimr_pool = -dnorm(lpred_pool) / (1 - pnorm(lpred_pool)),
+    rimr_sep = -dnorm(lpred_sep) / (1 - pnorm(lpred_sep)),
+
+    # generalized rediduals
+    gr_pool = ext_benefit_tl * imr_pool + (1 - ext_benefit_tl) * rimr_pool,
+    gr_sep = ext_benefit_tl * imr_sep + (1 - ext_benefit_tl) * rimr_sep,
   ) %>%
-  purrr::map(~ reduce(., full_join, by = c("vars", "stat"))) %>%
-  reduce(full_join, by = c("vars", "stat")) %>%
-  setNames(c("vars", "stat", paste0("reg", seq_len(length(xlist) * 2)))) %>%
-  bind_rows(
-    c(vars = "", stat = "", reg1 = "", reg2 = "",
-      reg3 = "", reg4 = "", reg5 = "", reg6 = ""),
-    .
-  )
+  select(-poolmod, -sepmod)
 
-ivfstat_ext <- list(ivest_xreport1_ext, ivest_xreport2_ext) %>%
-  purrr::map_depth(2, ~ fitstat(., ~ ivwald)$ivwald1$stat) %>%
-  purrr::map(~ as_vector(.)) %>%
-  as_vector() %>%
-  matrix(nrow = 1) %>%
-  data.frame() %>%
-  setNames(paste0("reg", seq_len(length(xlist) * 2)))
 
-ivfstat_ext_tab <- ivfstat_ext %>%
-  mutate_all(list(~ sprintf("%1.3f", .))) %>%
-  mutate(vars = "Fstat of IV", stat = "vars")
-
-list(ivest_xreport1_ext, ivest_xreport2_ext) %>%
-  flatten() %>%
-  regtab_fixest(
-    keep_coef = "int_price_benefit",
-    label_coef = list("fit_int_price_benefit" = "Report x log(first price)")
-  ) %>%
-  regtab_addline(list(waldtest_ext, ivfstat_tab, xlist_duptab)) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
-    align = "lcccccc"
-  ) %>%
-  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
-  kable_styling()
-
-#'
-#' intensive-margin elasticityのPanel IVの結果
-#'
-#' - 個人固定効果を除くと価格の上昇が寄付の増加を促す（個人固定効果を入れていないのが原因？何らかの解釈が可能？）
-#' - 個人固定効果を入れると統計的に非有意となる（弱操作変数の影響とは考えにくい）
 #'
 #+
-ivest_xreport1_int <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = log_total_g ~ .,
-    x = update(., ~ . - employee),
-    z = int_price_benefit ~ employee,
-    fixef = ~year, cluster = ~pid,
-    data = subset(df, i_ext_giving == 1 & year <= 2017)
-  ))
-
-ivest_xreport2_int <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = log_total_g ~ .,
-    x = update(., ~ . - employee),
-    z = int_price_benefit ~ employee,
-    fixef = ~ year + pid, cluster = ~pid,
-    data = subset(df, i_ext_giving == 1 & year <= 2017)
-  ))
-
-ivfstat_int <- list(ivest_xreport1_int, ivest_xreport2_int) %>%
-  purrr::map_depth(2, ~ fitstat(., ~ ivwald)$ivwald1$stat) %>%
-  purrr::map(~ as_vector(.)) %>%
-  as_vector() %>%
-  matrix(nrow = 1) %>%
-  data.frame() %>%
-  setNames(paste0("reg", seq_len(length(xlist) * 2)))
-
-ivfstat_int_tab <- ivfstat_int %>%
-  mutate_all(list(~ sprintf("%1.3f", .))) %>%
-  mutate(vars = "Fstat of IV", stat = "vars")
-
-list(ivest_xreport1_int, ivest_xreport2_int) %>%
-  flatten() %>%
-  regtab_fixest(
-    keep_coef = "int_price_benefit",
-    label_coef = list("fit_int_price_benefit" = "Report x log(first price)")
-  ) %>%
-  regtab_addline(list(ivfstat_int_tab, xlist_duptab)) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
-    align = "lcccccc"
-  ) %>%
-  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
-  kable_styling()
-
-
-#'
-#' ## Panel IV: Endog var is tax report and IV is employed dummy.
-#'
-#' $\log g_{it}$を寄付の対数値、$\ln p_{it}$は寄付の相対価格（first price）とする。
-#' $D_{it}$はtax reportのダミー変数とし、first-stageの予測値を$\hat{D}_{it}$とする。
-#' Panel IVの推定において、個人固定効果$\alpha$と時間固定効果$\lambda$はコントロールする。
-#' 仮説検定では、個人レベルでクラスターした標準誤差を使用する。
-#'
-#' Second stage:
-#'
-#' $$
-#' \log g_{it}
-#' = \alpha_{2i} + \delta \hat{D}_{it} \log p_{it} + X_{it} \beta_2
-#' + \lambda_{2t} + \epsilon_{it}
-#' $$
-#'
-#' First stage:
-#'
-#' $$
-#' D_{it}
-#' = \alpha_{1i} + \gamma \text{Employed}_{it} + X_{it} \beta_1
-#' + \lambda_{1t} + \eta_{it}
-#' $$
-#'
-#' Panel first-stageのメッセージ
-#' - 自営業者より労働者がtax reportする確率は約50%高い
-#' - 個人固定効果をコントロールしても統計的に有意（F-値は最低でも80）
-#'
-#+
-est_report1 <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = ext_benefit_tl ~ ., x = .,
-    fixef = ~ year, cluster = ~ pid,
-    data = subset(df, year <= 2017)
-  ))
-
-est_report2 <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = ext_benefit_tl ~ ., x = .,
-    fixef = ~year + pid, cluster = ~pid,
-    data = subset(df, year <= 2017)
-  ))
-
-wald_iv <- list(est_report1, est_report2) %>%
-  purrr::map_depth(2, ~ summary(.)$coeftable["employee", 3]^2) %>%
-  purrr::map( 
-    ~ c("F-stat of a dummy of employee", "stats", sprintf("%1.2f", as_vector(.)))
-  ) %>%
-  purrr::map( 
-    ~ setNames(., c("vars", "stat", paste0("reg", seq_len(length(xlist)))))
-  )
-
-est_report <- list(est_report1, est_report2) 
-
-1:2 %>%
-  purrr::map(
-    function(x)
-    est_report[[x]] %>%
-      regtab_fixest(keep_coef = "employee") %>%
-      regtab_addline(., list(xlist_tab, wald_iv[[x]]))
-  ) %>%
-  reduce(full_join, by = c("vars", "stat")) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
-    align = "lcccccc"
-  ) %>%
-  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
-  kable_styling()
-
-#'
-#' 上の表の(6)の予測値を$\hat{D}_{it}$とする。
-#+
-df$p_ext_benefit_tl <- predict(est_report2[[3]], df)
-
-#'
-#' Panel IVの推定結果のメッセージ
-#' - ITTとして推定した弾性値（約1%）より若干弾力的である
-#' - Tax reportをしていない人の価格は必ず$\log 1$であることを考えれば、納得できる結果
-#+
-ivest_report1 <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = log_total_g ~ .,
-    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
-    fixef = ~ year, cluster = ~ pid,
-    data = subset(df, year <= 2017)
-  ))
-
-ivest_report2 <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = log_total_g ~ .,
-    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
-    fixef = ~year + pid, cluster = ~ pid,
-    data = subset(df, year <= 2017)
-  ))
-
-list(ivest_report1, ivest_report2) %>%
-  flatten() %>%
-  regtab_fixest(
-    keep_coef = "log_price",
-    label_coef = list(
-      "log_price:p_ext_benefit_tl" = "Propensity of Report x log(first price)"
+sepstage1 %>%
+  pull(sepmod, name = year) %>%
+  list.merge(list("Pool" = poolstage1), .) %>%
+  modelsummary(
+    title = "Probit Estimation of Selection Equation",
+    coef_omit = "factor",
+    coef_rename = c(
+      "employee" = "1 = Wage earner",
+      "log_price" = "log(first giving price)",
+      "log_pinc_all" = "log(income)",
+      "sqage" = "Square of age"
+    ),
+    gof_omit = "R2|AIC|BIC",
+    stars = c("***" = .01, "**" = .05, "*" = .1),
+    add_rows = tribble(
+      ~"term", ~"Pool", ~"2012", ~"2013", ~"2014", ~"2015", ~"2016", ~"2017",
+      "Dummy of area", "X", "X", "X", "X", "X", "X", "X",
+      "Dummy of industry", "X", "X", "X", "X", "X", "X", "X"
     )
-  ) %>%
-  regtab_addline(list(xlist_duptab)) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
-    align = "lcccccc"
-  ) %>%
-  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
-  kable_styling()
+  )
 
-#'
-#' extensive-margin elasticityのPanel IVの結果
-#'
-#' - ITTとして想定したベースラインの結果（-1.4 ~ -1.2%）より、非弾力的になる（0.1%）
-#' - extensive-marginの弾力性はtax reportをしていない人に特に強い影響を与えるのか？
 #'
 #+
-ivest_report1_ext <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = i_ext_giving ~ .,
-    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
-    fixef = ~year, cluster = ~pid,
-    data = subset(df, year <= 2017)
-  ))
+stage2 <- list(
+  "(1)" = fixest::xpd(
+    log_total_g ~ ..stage1 | ext_benefit_tl:log_price ~ psc_pool:log_price
+  ),
+  "(2)" = fixest::xpd(
+    log_total_g ~ ..stage1 | ext_benefit_tl:log_price ~ psc_sep:log_price
+  ),
+  "(3)" = fixest::xpd(
+    log_total_g ~ ext_benefit_tl:log_price + gr_pool + ..stage1
+  ),
+  "(4)" = fixest::xpd(
+    log_total_g ~ ext_benefit_tl:log_price + gr_sep + ..stage1
+  ),
+  "(5)" = fixest::xpd(
+    log_total_g ~ ext_benefit_tl:log_price +
+      gr_pool + gr_pool:ext_benefit_tl + ..stage1
+  ),
+  "(6)" = fixest::xpd(
+    log_total_g ~ ext_benefit_tl:log_price +
+      gr_sep + gr_sep:ext_benefit_tl + ..stage1
+  )
+)
 
-ivest_report2_ext <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = i_ext_giving ~ .,
-    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
-    fixef = ~ year + pid, cluster = ~pid,
-    data = subset(df, year <= 2017)
-  ))
-
-waldtest_ext <- list(ivest_report1_ext, ivest_report2_ext) %>%
-  purrr::map_depth(2, ~ tibble(
-    vars = "Implied price elasticity",
-    coef = coef(.)["log_price:p_ext_benefit_tl"],
-    se = fixest::se(.)["log_price:p_ext_benefit_tl"],
-    invmu = 1 / mean(model.matrix(., type = "lhs")),
-    p = fixest::pvalue(.)["log_price:p_ext_benefit_tl"]
+stage2 %>%
+  purrr::map(~ fixest::feols(
+    ., cluster = ~ panelid,
+    data = subset(estdf, i_ext_giving == 1)
   )) %>%
-  purrr::map_depth(2, function(x)
-    x %>%
-      mutate(
-        coef = case_when(
-          p <= .01 ~ sprintf("%1.3f***", coef / invmu),
-          p <= .05 ~ sprintf("%1.3f**", coef / invmu),
-          p <= .1 ~ sprintf("%1.3f*", coef / invmu),
-          TRUE ~ sprintf("%1.3f", coef / invmu)
-        ),
-        se = sprintf("(%1.3f)", se / invmu)
-      ) %>%
-      dplyr::select(-p, -invmu) %>%
-      pivot_longer(-vars, names_to = "stat", values_to = "val")
-  ) %>%
-  purrr::map(~ reduce(., full_join, by = c("vars", "stat"))) %>%
-  reduce(full_join, by = c("vars", "stat")) %>%
-  setNames(c("vars", "stat", paste0("reg", seq_len(length(xlist) * 2)))) %>%
-  bind_rows(
-    c(vars = "", stat = "", reg1 = "", reg2 = "",
-      reg3 = "", reg4 = "", reg5 = "", reg6 = ""),
-    .
-  )
-
-list(ivest_report1_ext, ivest_report2_ext) %>%
-  flatten() %>%
-  regtab_fixest(
-    keep_coef = "log_price",
-    label_coef = list(
-      "log_price:p_ext_benefit_tl" = "Propensity of Report x log(first price)"
+  modelsummary(
+    title = paste(
+      "Population First-Price Elasticities:",
+      "Pooled 2SLS and Control Function Approach"
+    ),
+    coef_map = c(
+      "fit_ext_benefit_tl:log_price" =
+        "Propensity x log(first giving price)",
+      "ext_benefit_tl:log_price" =
+        "Applying tax relief x log(first giving price)",
+      "log_pinc_all" = "log(income)",
+      "gr_pool" = "Generalized residuals (pool)",
+      "ext_benefit_tl:gr_pool" =
+        "Applying tax relief x Generalized residuals (pool)",
+      "gr_sep" = "Generalized residuals (separate)",
+      "ext_benefit_tl:gr_sep" =
+        "Applying tax relief x Generalized residuals (separate)"
+    ),
+    gof_omit = "R2 Pseudo|R2 Within|AIC|BIC|Log|Std",
+    stars = c("***" = .01, "**" = .05, "*" = .1),
+    add_rows = tribble(
+      ~"term", ~"(1)", ~"(2)", ~"(3)", ~"(4)", ~"(5)", ~"(6)",
+      "Dummy of area", "X", "X", "X", "X", "X", "X",
+      "Dummy of industry", "X", "X", "X", "X", "X", "X",
+      "Square of age", "X", "X", "X", "X", "X", "X"
     )
   ) %>%
-  regtab_addline(list(waldtest_ext, xlist_duptab)) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
-    align = "lcccccc"
-  ) %>%
-  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
-  kable_styling()
+  kableExtra::add_header_above(c(
+    " " = 1, "Pooled 2SLS" = 2, "CF Approach" = 4
+  ))
 
-#'
-#' intensive-margin elasticityのPanel IVの結果
-#'
-#' - 個人固定効果を加えると、ITTとして想定した弾性値（-1 ~ -0.5%）と似たような結果を取る
-#' - Intenstive-margin elasticityはtax reportをする人に特に影響を与えるのか？
-#'
+# /*
 #+
-ivest_report1_int <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = log_total_g ~ .,
-    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
-    fixef = ~year, cluster = ~pid,
-    data = subset(df, i_ext_giving == 1 & year <= 2017)
-  ))
-
-ivest_report2_int <- xlist %>%
-  purrr::map(~ fit_fixest(
-    y = log_total_g ~ .,
-    x = update(., ~ . - employee + log_price:p_ext_benefit_tl),
-    fixef = ~ year + pid, cluster = ~pid,
-    data = subset(df, i_ext_giving == 1 & year <= 2017)
-  ))
-
-list(ivest_report1_int, ivest_report2_int) %>%
-  flatten() %>%
-  regtab_fixest(
-    keep_coef = "log_price",
-    label_coef = list(
-      "log_price:p_ext_benefit_tl" = "Propensity of Report x log(first price)"
-    )
-  ) %>%
-  regtab_addline(list(xlist_duptab)) %>%
-  mutate(vars = if_else(stat == "se", "", vars)) %>%
-  dplyr::select(-stat) %>%
-  kable(
-    col.names = c("", sprintf("(%1d)", seq_len(length(xlist) * 2))),
-    align = "lcccccc"
-  ) %>%
-  add_header_above(c("", "w/o individual FE" = 3, "w/ individual FE" = 3)) %>%
-  kable_styling()
+rmarkdown::render(
+  "script/R/4-paneliv.r",
+  output_dir = "report/view"
+)
+# */
