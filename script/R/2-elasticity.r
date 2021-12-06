6def77c3f3d1c652ae0b4b3cac9177561e931f23
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-12-06 23:35:01 +0900

    弾力性推定の新規スクリプトを作成 - #129

---
+++ script/R/2-elasticity.r
@@ -0,0 +1,69 @@
+#' ---
+#' title: |
+#'   Price Elasticity of Charitable Giving
+#' author: Hiroki Kato
+#' output:
+#'   bookdown::html_document2:
+#'     toc: true
+#'     toc_float: true
+#'     number_sections: false
+#' params:
+#'   preview: true
+#' ---
+#'
+#+ include = FALSE, eval = params$preview
+knitr::opts_chunk$set(
+  message = FALSE,
+  warning = FALSE,
+  echo = FALSE,
+  cache = FALSE,
+  include = TRUE,
+  fig.width = 10
+)
+
+library(here)
+knitr::opts_knit$set(
+  root.dir = here::here()
+)
+
+options(
+  knitr.kable.NA = " ",
+  knitr.table.format = "html",
+  modelsummary_stars_note = FALSE
+)
+
+
+#'
+#+ include = FALSE, eval = params$preview
+library(xfun)
+xfun::pkg_attach2(c(
+  "tidyverse", "rlist", "modelsummary", "kableExtra",
+  "estimatr", "fixest"
+))
+
+lapply(Sys.glob(file.path("script/R/functions", "*.r")), source)
+
+#'
+#+ include = FALSE, eval = params$preview
+df <- readr::read_csv(
+  "data/shaped2.csv",
+  col_types = cols(
+    ext_credit_giving = col_double(),
+    krw_credit_giving = col_double(),
+    trust_politician = col_double(),
+    political_pref = col_double(),
+    addtax = col_double(),
+    avg_welfare_tax = col_double(),
+    opt_welfare_tax = col_double(),
+    now_balance = col_double(),
+    ideal_balance = col_double()
+  )
+) %>%
+dplyr::filter(
+  ext_benefit_tl == 0 | (ext_benefit_tl == 1 & i_ext_giving == 1)
+)
+
+#'
+#' # Price Elasticity of Charitable Giving
+#'
+#+
