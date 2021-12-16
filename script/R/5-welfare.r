03445701a7bf3d2765c820ee4c43416d5ddf15a5
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-12-17 01:03:33 +0900

    welfareのスクリプトを修正 - #130

--- script/R/5-welfare.r
+++ script/R/5-welfare.r
@@ -47,7 +47,7 @@ lapply(Sys.glob(file.path("script/R/functions", "*.r")), source)
 #'
 #+ include = FALSE, eval = params$preview
 df <- readr::read_csv(
-  "data/shaped2.csv",
+  "data/shaped2_propensity.csv",
   col_types = cols(
     ext_credit_giving = col_double(),
     krw_credit_giving = col_double(),
@@ -69,117 +69,21 @@ dplyr::filter(
 #'
 #+ include = FALSE, eval = params$preview
 fixest::setFixest_fml(
-  ..stage1 = ~ employee +
-    log_price + log_pinc_all +
-    age + sqage + gender + univ + highschool +
-    factor(industry) + factor(area)
+  ..stage2 = ~ log_pinc_all + sqage | panelid + year + industry + area
 )
 
-#'
-#+ eval = FALSE
-meandf <- df %>%
-  dplyr::select(all.vars(fixest::xpd(~ ..stage1)), panelid) %>%
-  fastDummies::dummy_cols(
-    select_columns = "industry",
-    remove_selected_columns = TRUE
-  ) %>%
-  fastDummies::dummy_cols(
-    select_columns = "area",
-    remove_selected_columns = TRUE
-  ) %>%
-  select(-"industry_-9", -industry_NA) %>%
-  group_by(panelid) %>%
-  summarize_all(list(mean = ~ sum(., na.rm = TRUE) / 6))
-
-mundlak <- meandf %>%
-  select(-panelid) %>%
-  names %>%
-  paste(collapse = "+") %>%
-  paste("~", ., sep = "") %>%
-  formula
-
-fixest::setFixest_fml(..mundlak = mundlak)
-
-#'
-#+ eval = params$preview
-est_stage1_pool <- df %>%
-  # left_join(meandf, by = "panelid") %>%
-  dplyr::filter(year <= 2017) %>%
-  fixest::feglm(
-    fixest::xpd(ext_benefit_tl ~ ..stage1 | year),
-    family = binomial(link = "probit"),
-    data = .
-  )
-
-est_stage1_sep <- df %>%
-  # left_join(meandf, by = "panelid") %>%
-  dplyr::filter(year <= 2017) %>%
-  group_by(year) %>%
-  do(sepmod = fixest::feglm(
-    fixest::xpd(ext_benefit_tl ~ ..stage1),
-    family = binomial(link = "probit"),
-    data = .
-  ))
-
-estdf <- df %>%
-  # left_join(meandf, by = "panelid") %>%
-  dplyr::filter(year <= 2017 & !is.na(ext_benefit_tl)) %>%
-  mutate(poolmod = list(est_stage1_pool)) %>%
-  left_join(est_stage1_sep, by = "year") %>%
-  group_by(year) %>%
-  do(modelr::spread_predictions(
-    ., first(.$poolmod), first(.$sepmod),
-    type = "link"
-  )) %>%
-  rename(lpred_pool = "first(.$poolmod)", lpred_sep = "first(.$sepmod)") %>%
-  mutate(
-    # propensity score
-    psc_pool = pnorm(lpred_pool), psc_sep = pnorm(lpred_sep),
-
-    # inverse mills ratio for R = 1
-    imr1_pool = dnorm(lpred_pool) / pnorm(lpred_pool),
-    imr1_sep = dnorm(lpred_sep) / pnorm(lpred_sep),
-
-    # inverse mills ratio for R = 0
-    imr0_pool = dnorm(lpred_pool) / (1 - pnorm(lpred_pool)),
-    imr0_sep = dnorm(lpred_sep) / (1 - pnorm(lpred_sep)),
-
-    # generalized error
-    gr_pool = if_else(ext_benefit_tl == 1, imr1_pool, imr0_pool),
-    gr_sep = if_else(ext_benefit_tl == 1, imr1_sep, imr0_sep)
-  ) %>%
-  ungroup() %>%
-  select(-poolmod, -sepmod)
-
-#'
-#+ eval = params$preview
-fixest::setFixest_fml(
-  ..stage2 = ~ log_pinc_all +
-    age + sqage + gender + univ + highschool +
-    factor(industry) + factor(area)
+stage2r1 <- list(
+  "(1)" = log_total_g ~ log_price + ..stage2,
+  "(2)" = log_total_g ~ log_price + gr_sep + ..stage2,
+  "(3)" = log_total_g ~ log_price + gr_pool + ..stage2
 )
-
-stage2r1 <- estdf %>%
-  dplyr::filter(ext_benefit_tl == 1) %>%
-  {
-    list(
-      "(1)" = list(
-        mod = log_total_g ~ log_price + ..stage2 | panelid + year,
-        data = .
-      ),
-      "(2)" = list(
-        mod = log_total_g ~ log_price + ..stage2 + gr_sep | panelid + year,
-        data = .
-      ),
-      "(3)" = list(
-        mod = log_total_g ~ log_price + ..stage2 + gr_pool | panelid + year,
-        data = .
-      )
-    )
-  }
 
 est_stage2r1 <- stage2r1 %>%
-  purrr::map(~ feols(xpd(.$mod), data = .$data, cluster = ~panelid))
+  purrr::map(~ feols(
+    xpd(.),
+    data = subset(df, ext_benefit_tl == 1),
+    cluster = ~panelid
+  ))
 
 #'
 #' # Welfare Implication
@@ -288,7 +192,7 @@ elast %>%
 # /*
 #+
 rmarkdown::render(
-  "script/R/4-welfare.r",
+  "script/R/5-welfare.r",
   output_dir = "report/view"
 )
 # */
