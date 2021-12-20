ae1aa25537b7f303b7ad3f5dc265ecdc3c50b736
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2021-12-20 16:41:10 +0900

    1-summary.rを修正 - #130

--- script/R/1-summary.r
+++ script/R/1-summary.r
@@ -67,7 +67,7 @@ dplyr::filter(
 )
 
 #'
-#' The National Survey of Tax and Benefit (hereafter, NaSTab) is
+#' The National Survey of Tax and Benefit (hereafter, NaSTaB) is
 #' an annual financial panel survey
 #' implemented by The Korea Institute of Taxation and Finance
 #' to study the tax burden of households and the benefits
@@ -98,11 +98,12 @@ dplyr::filter(
 #'
 #+ SummaryCovariate
 df %>%
+  dplyr::filter(year <= 2017) %>%
   datasummary(
     (`Annual chariatable giving (unit: 10,000KRW)` = i_total_giving) +
     (`Dummary of donation > 0` = i_ext_giving) +
     (`Annual taxable labor income (unit: 10,000KRW)` = lincome) +
-    (`First giving relative price` = price) +
+    (`Relative first price of giving` = price) +
     (`Dummy of declaration of a tax relief` = ext_benefit_tl) +
     (`Age` = age) +
     (`Female dummy` = gender) +
@@ -126,15 +127,88 @@ df %>%
   kableExtra::pack_rows("Income, giving price, and tax report", 3, 5) %>%
   kableExtra::pack_rows("Individual Characteristics", 6, 12)
 
+#+ SummaryPrice, fig.cap = "Income Distribution and Relative Giving Price in 2013. Notes: The left and right axis measure the relative frequency of respondents and the relative giving price, respectively. A blue step line and a red dashed horizontal line represents the giving price in 2013 and 2014, respectively. The grey bar shows income distribution in 2013.", out.width = "85%", out.extra = ""
+df %>%
+  filter(year == 2013) %>%
+  dplyr::select(lincome, price) %>%
+  ggplot(aes(x = lincome)) +
+  geom_histogram(
+    aes(y = ..count.. / sum(..count..), fill = "Relative frequency"),
+    color = "black"
+  ) +
+  geom_step(
+    aes(y = price * 0.5, color = "Giving Price in 2013"),
+    size = 1
+  ) +
+  geom_hline(
+    aes(yintercept = (1 - 0.15) * 0.5),
+    color = "red", linetype = 2, size = 1
+  ) +
+  scale_color_manual(NULL, values = "blue") +
+  scale_fill_manual(NULL, values = "grey80") +
+  scale_y_continuous(
+    breaks = seq(0, 0.5, 0.125),
+    sec.axis = sec_axis(~ . / 0.5, name = "Giving Price")
+  ) +
+  scale_x_continuous(breaks = c(1200, 4600, 8800, 30000)) +
+  labs(
+    x = "Annual taxable income (10,000KRW)",
+    y = "Relative Frequency",
+    caption = "Red dashed line is the giving price after the 2014 tax reform."
+  ) +
+  ggtemp(size = list(title = 15, text = 13, caption = 13))
+
 #'
-#+ SummaryOutcome, fig.cap = "Proportion of Donors and Average Donations among Donors. Notes: The left and right axises measure prooortion of donors and the average amount of donations among donors, respectively. Authors made this graph based on NaSTaB data.", out.width = "85%", out.extra = ""
+#' NaSTaB asks respondents to answer the annual labor income last year.
+#' In our sample,
+#' the average annual taxable income is 17.54 million KRW (17,540 USD)
+#' (See \@ref(tab:SummaryCoveriate)).
+#' According to the National Tax Statistical Yearbook
+#' published by Korean National Tax Service,
+#' the average annual taxable income is
+#' 32.77 million KRW (32,770 USD) from 2012 to 2018
+#' for employees who submitted the tax return.[^source]
+#' Since our sample includes subjects with no labor income, such as housewives,
+#' our sample mean of income is lower than
+#' the average income calculated by the public organizations.
+#' In Figure \@ref(fig:SummaryPrice),
+#' the gray bars show the distribution of annual taxable income in 2013.
+#' The income distribution is right-skewed.
+#'
+#' [^source]: URL
+#'
+#' Using this variable,
+#' we construct the relative price of giving
+#' under the tax deduction system (2012 and 2013).[^fprice]
+#' After the tax reform (after 2014),
+#' the relative price of giving is 0.85 regardless of labor income,
+#' as we explained in Section \@ref(taxreform).
+#' In Figure \@ref(fig:SummaryPrice),
+#' the blue line shows the giving price in 2012 and 2013,
+#' while the red dashed line shows the giving price after 2014.
+#' The 2014 tax reform has expanded tax incentives
+#' (has decreased the relative price of giving)
+#' for those whose annual income is less than 120 million KRW
+#' (120,000 USD) in 2013,
+#' while reducing tax incentives
+#' (has increased the relative price of giving)
+#' for those whose annual income is greater than
+#' 460 million KRW (460,000 USD) in 2013.
+#' The tax incentive has been unchanged for
+#' those whose annual income is between 120 million KRW and 460 million KRW
+#' due to the 2014 tax reform.
+#' This is main source of identification for effect of tax incentive on giving.
+#'
+#' [^fprice]: The giving price shown in Table \@ref(tab:SummaryCovariate) is the *first* giving price. The giving price can be manipulated by an amount of donation. To avoid this endogeneity, we use the giving price where the amount of donation is zero. We will discuss this issue in the next section.
+#'
+#+ SummaryGiving, fig.cap = "Proportion of Donors and Average Donations among Donors. Notes: The left and right axises measure prooortion of donors and the average amount of donations among donors, respectively. Authors made this graph based on NaSTaB data.", out.width = "85%", out.extra = "", eval = FALSE
 df %>%
   mutate(
     i_total_giving = if_else(i_ext_giving == 1, i_total_giving, NA_real_)
   ) %>%
   group_by(year) %>%
   summarize_at(
-    vars(i_ext_giving, i_total_giving), list(~mean(., na.rm = TRUE))
+    vars(i_ext_giving, i_total_giving), list(~ mean(., na.rm = TRUE))
   ) %>%
   ggplot(aes(x = year)) +
   geom_bar(
@@ -166,6 +240,119 @@ df %>%
   scale_x_continuous(breaks = seq(2012, 2018, 1)) +
   labs(x = "Year", y = "Proportion of Donors") +
   ggtemp(size = list(title = 15, text = 13))
+
+#'
+#+ SummaryGivingOverall, fig.cap = "Average Logged Giving by Three Income Groups. Notes: We created three income groups, with the relative price of giving rising (circle), unchanged (triangle), and falling (square) between 2013 and 2014. The group averages are normalized to be zero in 2013.", out.width = "85%", out.extra = ""
+df %>%
+  mutate(group = case_when(
+    credit_loss == 1 ~ 3,
+    credit_neutral == 1 ~ 2,
+    credit_benefit == 1 ~ 1
+  )) %>%
+  dplyr::filter(year <= 2017) %>%
+  dplyr::filter(!is.na(group)) %>%
+  group_by(year, group) %>%
+  summarize(mu = mean(log_total_g, na.rm = TRUE)) %>%
+  tidyr::pivot_wider(names_from = "year", values_from = "mu") %>%
+  mutate(base = `2013`) %>%
+  dplyr::select(group, base, everything()) %>%
+  tidyr::pivot_longer(-(group:base), values_to = "mu", names_to = "year") %>%
+  mutate(mu = mu - base, year = as.numeric(year)) %>%
+  mutate(group = factor(
+    group,
+    labels = c("< 1200", "[1200, 4600]", "> 4600")
+  )) %>%
+  ggplot(aes(x = year, y = mu, group = group)) +
+  geom_vline(aes(xintercept = 2013.5), linetype = 3) +
+  geom_hline(aes(yintercept = 0), linetype = 3) +
+  geom_point(aes(shape = group), size = 4) +
+  geom_line() +
+  scale_x_continuous(breaks = seq(2012, 2018, 1)) +
+  labs(
+    x = "Year",
+    y = "Difference of average logged giving",
+    shape = "Income group (unit:10,000KRW)",
+    caption = paste(
+      "The difference is calculated by",
+      "(mean of logged donation in year t) - (mean of logged donation in 2013)."
+    )
+  ) +
+  ggtemp(size = list(title = 15, text = 13, caption = 13))
+
+#+ SummaryGivingIntensive, fig.cap = "Average Logged Giving by Three Income Groups Conditional on Donors. Notes: We created three income groups, with the relative price of giving rising (circle), unchanged (triangle), and falling (square) between 2013 and 2014. The group averages are normalized to be zero in 2013.", out.width = "85%", out.extra = ""
+df %>%
+  mutate(group = case_when(
+    credit_loss == 1 ~ 3,
+    credit_neutral == 1 ~ 2,
+    credit_benefit == 1 ~ 1
+  )) %>%
+  dplyr::filter(year <= 2017) %>%
+  dplyr::filter(!is.na(group) & i_ext_giving == 1) %>%
+  group_by(year, group) %>%
+  summarize(mu = mean(log_total_g, na.rm = TRUE)) %>%
+  tidyr::pivot_wider(names_from = "year", values_from = "mu") %>%
+  mutate(base = `2013`) %>%
+  dplyr::select(group, base, everything()) %>%
+  tidyr::pivot_longer(-(group:base), values_to = "mu", names_to = "year") %>%
+  mutate(mu = mu - base, year = as.numeric(year)) %>%
+  mutate(group = factor(
+    group,
+    labels = c("< 1200", "[1200, 4600]", "> 4600")
+  )) %>%
+  ggplot(aes(x = year, y = mu, group = group)) +
+  geom_vline(aes(xintercept = 2013.5), linetype = 3) +
+  geom_hline(aes(yintercept = 0), linetype = 3) +
+  geom_point(aes(shape = group), size = 4) +
+  geom_line() +
+  scale_x_continuous(breaks = seq(2012, 2018, 1)) +
+  labs(
+    x = "Year",
+    y = "Difference of average logged giving",
+    shape = "Income group (unit:10,000KRW)",
+    caption = paste(
+      "The difference is calculated by",
+      "(proportion of donors in year t) - (proportion of donors in 2013)."
+    )
+  ) +
+  ggtemp(size = list(title = 15, text = 13, caption = 13))
+
+#'
+#+ SummaryGivingExtensive, fig.cap = "Proportion of Donors by Three Income Groups. Notes: We created three income groups, with the relative price of giving rising (circle), unchanged (triangle), and falling (square) between 2013 and 2014. The group averages are normalized to be zero in 2013.", out.width = "85%", out.extra = ""
+df %>%
+  mutate(group = case_when(
+    credit_loss == 1 ~ 3,
+    credit_neutral == 1 ~ 2,
+    credit_benefit == 1 ~ 1
+  )) %>%
+  dplyr::filter(year <= 2017) %>%
+  dplyr::filter(!is.na(group)) %>%
+  group_by(year, group) %>%
+  summarize(mu = mean(i_ext_giving, na.rm = TRUE)) %>%
+  tidyr::pivot_wider(names_from = "year", values_from = "mu") %>%
+  mutate(base = `2013`) %>%
+  dplyr::select(group, base, everything()) %>%
+  tidyr::pivot_longer(-(group:base), values_to = "mu", names_to = "year") %>%
+  mutate(mu = mu - base, year = as.numeric(year)) %>%
+  mutate(group = factor(
+    group,
+    labels = c("< 1200", "[1200, 4600]", "> 4600")
+  )) %>%
+  ggplot(aes(x = year, y = mu, group = group)) +
+  geom_vline(aes(xintercept = 2013.5), linetype = 3) +
+  geom_hline(aes(yintercept = 0), linetype = 3) +
+  geom_point(aes(shape = group), size = 4) +
+  geom_line() +
+  scale_x_continuous(breaks = seq(2012, 2018, 1)) +
+  labs(
+    x = "Year",
+    y = "Difference of proportion of donors",
+    shape = "Income group (unit:10,000KRW)",
+    caption = paste(
+      "The difference is calculated by",
+      "(proportion of donors in year t) - (proportion of donors in 2013)."
+    )
+  ) +
+  ggtemp(size = list(title = 15, text = 13, caption = 13))
 
 #'
 #+ SummaryOutcome2, fig.cap = "Distribution of Charitable Giving among Those Who Donated", out.width = "85%", out.extra = ""
@@ -230,75 +417,6 @@ df %>%
 #'
 #' [^Question]: Respondents answer the amount of donation for seven specific purposes last year. Seven specific purposes are policitical parties, educational organizations, social welfare organizations, organizations for culutre and art, religious groups, charity activies organaized by religious group, other purposes. We sum up the amount of donations, and consider it as the annual charitable giving.
 #'
-#+ SummaryPrice, fig.cap = "Income Distribution and Relative Giving Price in 2013. Notes: The left and right axis measure the relative frequency of respondents and the relative giving price, respectively. A blue step line and a red dashed horizontal line represents the giving price in 2013 and 2014, respectively. The grey bar shows income distribution in 2013.", out.width = "85%", out.extra = ""
-df %>%
-  filter(year == 2013) %>%
-  dplyr::select(lincome, price) %>%
-  ggplot(aes(x = lincome)) +
-  geom_histogram(
-    aes(y = ..count.. / sum(..count..), fill = "Relative frequency"),
-    color = "black"
-  ) +
-  geom_step(
-    aes(y = price * 0.5, color = "Giving Price in 2013"),
-    size = 1
-  ) +
-  geom_hline(
-    aes(yintercept = (1 - 0.15) * 0.5),
-    color = "red", linetype = 2, size = 1
-  ) +
-  scale_color_manual(NULL, values = "blue") +
-  scale_fill_manual(NULL, values = "grey80") +
-  scale_y_continuous(
-    breaks = seq(0, 0.5, 0.125),
-    sec.axis = sec_axis(~ . / 0.5, name = "Giving Price")
-  ) +
-  scale_x_continuous(breaks = c(1200, 4600, 8800, 30000)) +
-  labs(
-    x = "Annual taxable income (10,000KRW)",
-    y = "Relative Frequency",
-    caption = "Red dashed line is the giving price after the 2014 tax reform."
-  ) +
-  ggtemp(size = list(title = 15, text = 13, caption = 13))
-
-#'
-#' The second panel of Table \@ref(tab:SummaryCovariate)
-#' shows variables about income, tax report, and the giving price.
-#' NaSTaB asks respondents to answer the annual labor income last year.
-#' In our sample,
-#' the average annual taxable income is 18.76 million KRW (18,760 USD).
-#' According to the National Tax Statistical Yearbook
-#' published by Korean National Tax Service,
-#' the average annual taxable income is
-#' 32.77 million KRW (32,770 USD) from 2012 to 2018
-#' for employees who submitted the tax return.
-#' Since our sample includes subjects with no labor income, such as housewives,
-#' our sample mean of income is lower than
-#' the average income calculated by the public organizations.
-#' In Figure \@ref(fig:SummaryPrice),
-#' the gray bars show the distribution of annual taxable income in 2013.
-#' The income distribution is right-skewed.
-#'
-#' Using this variable,
-#' we construct the giving price
-#' under the tax deduction system (2012 and 2013).[^fprice]
-#' After the tax reform (after 2014),
-#' the giving price is 0.85 regardless of labor income,
-#' as we explained in Section \@ref(taxreform).
-#' In Figure \@ref(fig:SummaryPrice),
-#' the blue line shows the giving price in 2012 and 2013,
-#' while the red dashed line shows the giving price after 2014.
-#' From this figure,
-#' those whose annual income is less than 120,000,000 KRW
-#' (120,000 USD) in 2013 could receive benefit from the 2014 tax reform
-#' because the tax reform decreases the giving price.
-#' On the other hand,
-#' those whose annual income is greater than
-#' 460,000,000 KRW (460,000 USD) in 2013 had a loss by the 2014 tax reform
-#' since the tax reform increases the giving price.
-#'
-#' [^fprice]: The giving price shown in Table \@ref(tab:SummaryCovariate) is the *first* giving price. The giving price can be manipulated by an amount of donation. To avoid this endogeneity, we use the giving price where the amount of donation is zero. We will discuss this issue in the next section.
-#'
 #+ SummaryRelief, fig.cap = "Share of Tax Relief. Notes: A solid line is the share of applying for tax relief among wage eaners. A dashed line is the share of applying for tax relief other than wage earners.", out.width = "85%", out.extra = ""
 df %>%
   dplyr::filter(year <= 2017) %>%
@@ -360,82 +478,6 @@ df %>%
     shape = "Income group (unit:10,000KRW)"
   ) +
   ggtemp(size = list(title = 15, text = 13, caption = 13))
-
-#'
-#+ SummaryOutcome4, fig.cap = "Average Logged Giving in Three Income Groups. Notes: We created three income groups, with the relative price of giving rising (circle), unchanged (triangle), and falling (square) between 2013 and 2014.", out.width = "85%", out.extra = ""
-df %>%
-  mutate(group = case_when(
-    credit_loss == 1 ~ 3,
-    credit_neutral == 1 ~ 2,
-    credit_benefit == 1 ~ 1
-  )) %>%
-  dplyr::filter(year <= 2017) %>%
-  dplyr::filter(!is.na(group) & i_ext_giving == 1) %>%
-  group_by(year, group) %>%
-  summarize(mu = mean(log_total_g, na.rm = TRUE)) %>%
-  tidyr::pivot_wider(names_from = "year", values_from = "mu") %>%
-  mutate(base = `2013`) %>%
-  dplyr::select(group, base, everything()) %>%
-  tidyr::pivot_longer(-(group:base), values_to = "mu", names_to = "year") %>%
-  mutate(mu = mu / base, year = as.numeric(year)) %>%
-  mutate(group = factor(
-    group,
-    labels = c("< 1200", "[1200, 4600]", "> 4600")
-  )) %>%
-  ggplot(aes(x = year, y = mu, group = group)) +
-  geom_point(aes(shape = group), size = 4) +
-  geom_line() +
-  geom_vline(aes(xintercept = 2013.5), linetype = 3) +
-  geom_hline(aes(yintercept = 1), linetype = 3) +
-  scale_x_continuous(breaks = seq(2012, 2018, 1)) +
-  labs(
-    x = "Year",
-    y = "The ratio of mean donations (logged value)",
-    shape = "Income group (unit:10,000KRW)",
-    caption = paste(
-      "The ratio is calculated by",
-      "(mean of logged donation in year t) / (mean of logged donation in 2013)."
-    )
-  ) +
-  ggtemp(size = list(title = 15, text = 13, caption = 13))
-
-#+ SummaryOutcome5, fig.cap = "Proportion of Donors in Three Income Groups. Notes: We created three income groups, with the relative price of giving rising (circle), unchanged (triangle), and falling (square) between 2013 and 2014.", out.width = "85%", out.extra = ""
-df %>%
-  mutate(group = case_when(
-    credit_loss == 1 ~ 3,
-    credit_neutral == 1 ~ 2,
-    credit_benefit == 1 ~ 1
-  )) %>%
-  dplyr::filter(year <= 2017) %>%
-  dplyr::filter(!is.na(group)) %>%
-  group_by(year, group) %>%
-  summarize(mu = mean(i_ext_giving, na.rm = TRUE)) %>%
-  tidyr::pivot_wider(names_from = "year", values_from = "mu") %>%
-  mutate(base = `2013`) %>%
-  dplyr::select(group, base, everything()) %>%
-  tidyr::pivot_longer(-(group:base), values_to = "mu", names_to = "year") %>%
-  mutate(mu = mu / base, year = as.numeric(year)) %>%
-  mutate(group = factor(
-    group,
-    labels = c("< 1200", "[1200, 4600]", "> 4600")
-  )) %>%
-  ggplot(aes(x = year, y = mu, group = group)) +
-  geom_point(aes(shape = group), size = 4) +
-  geom_line() +
-  geom_vline(aes(xintercept = 2013.5), linetype = 3) +
-  geom_hline(aes(yintercept = 1), linetype = 3) +
-  scale_x_continuous(breaks = seq(2012, 2018, 1)) +
-  labs(
-    x = "Year",
-    y = "The ratio of proportion of donors",
-    shape = "Income group (unit:10,000KRW)",
-    caption = paste(
-      "The ratio is calculated by",
-      "(proportion of donors in year t) / (proportion of donors in 2013)."
-    )
-  ) +
-  ggtemp(size = list(title = 15, text = 13, caption = 13))
-
 
 # /*
 #+
