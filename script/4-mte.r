5edb3932ef322debebc9b105b20c75ce490b2d66
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-12-27 05:16:18 +0900

    MTE推定スクリプトの用意 - #133

---
+++ script/4-mte.r
@@ -0,0 +1,55 @@
+#' ---
+#' title: Marginal Treatment Effect
+#' author: Hiroki Kato
+#' output:
+#'   html_document:
+#'     toc: true
+#'     toc_float: true
+#' params:
+#'   preview: true
+#' ---
+#'
+#+ include = FALSE, eval = params$preview
+knitr::opts_chunk$set(
+  message = FALSE,
+  warning = FALSE,
+  echo = FALSE,
+  cache = FALSE,
+  include = TRUE,
+  fig.width = 10
+)
+
+library(here)
+knitr::opts_knit$set(
+  root.dir = here::here()
+)
+
+options(
+  knitr.kable.NA = " ",
+  knitr.table.format = "html",
+  modelsummary_stars_note = FALSE
+)
+
+
+#'
+#+ include = FALSE, eval = params$preview
+library(xfun)
+xfun::pkg_attach2(c(
+  "tidyverse", "rlist", "modelsummary", "kableExtra",
+  "estimatr", "fixest"
+))
+
+lapply(Sys.glob(file.path("script/R/functions", "*.r")), source)
+
+#'
+#+
+df <- readr::read_csv("data/shaped2.csv")
+
+#'
+# /*
+#+
+rmarkdown::render(
+  "script/R/1-summary.r",
+  output_dir = "report/view"
+)
+# */
