6f1eca1965b2eab1b827c8368109d137ac079283
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2022-04-26 17:20:25 +0900

    世代主ダミーと専業主婦ダミーの導入 - #147

--- R/0-preprocess.r
+++ R/0-preprocess.r
@@ -1,10 +1,10 @@
 #'
 #' raw data: data/merge/merge.dta
 #+
-library(xfun)
-xfun::pkg_attach2(c("tidyverse", "rlist"))
+library(here)
+source(here("R", "_library.r"))
 
-raw <- haven::read_dta("data/merge/merge.dta", encoding = "utf-8") %>%
+raw <- haven::read_dta(here("data/merge/merge.dta"), encoding = "utf-8") %>%
   as_tibble
 
 #'
@@ -12,18 +12,17 @@ raw <- haven::read_dta("data/merge/merge.dta", encoding = "utf-8") %>%
 #+
 ses <- raw %>%
   dplyr::select(
-    hhid, pid, year, wave, p_page, p_pedu, p_pgen, h_b10,
+    hhid, pid, year, wave, p_page, p_pedu, p_pgen, p_prel, h_b10,
     pca201:pca228, pinc_all, inc_bb1,
-    pga001, pgb110, pgb120, pgb151:pgb159, pgb051:pgb058, pgb090,
-    pgc007, pgc008, pea002, pgb020, pga020,
-    p_aa005, paa008, pda207, pda209
+    p_aa005, p_aa200, paa008, pda207, pda209
   ) %>%
   dplyr::rename(
     age = p_page, #年齢
     educ = p_pedu, #学歴
     sex = p_pgen, #性別
     area = h_b10, #地域コード
-    workpos = p_aa005, #労働形態
+    position = p_aa005, #従事地位（1=常用、2=臨時、3=自営業、4=無給の家族従事者）
+    working = p_aa200, #就業状態（1=就職、2=専業主婦、3=無職、4=学生）
     indust = paa008, #産業
     d_deduct_linc = pca201, #労働所得控除の有無
     deduct_linc = pca202, #労働所得控除額
@@ -35,35 +34,6 @@ ses <- raw %>%
     d_credit_donate_tinc = pda209, #総合所得の寄付金税額控除の有無
     tinc = pinc_all, #昨年1年間の総合所得
     linc = inc_bb1, #昨年1年間の労働所得
-
-    trust = pga001, #政治家への信頼
-    avg_balance = pgb110, #平均的な税負担と福祉水準
-    opt_balance = pgb120, #望ましい税負担と福祉水準
-
-    opttax_tinc_1 = pgb151, #所与の総合所得に対する適切な税率
-    opttax_tinc_3 = pgb152,
-    opttax_tinc_5 = pgb153,
-    opttax_tinc_7 = pgb154,
-    opttax_tinc_10 = pgb155,
-    opttax_tinc_20 = pgb156,
-    opttax_tinc_30 = pgb157,
-    opttax_tinc_50 = pgb158,
-    opttax_tinc_100 = pgb159,
-
-    opttax_linc_15 = pgb051, #所与の勤労所得に対する所与の税額が望ましいかどうか（2016年）
-    opttax_linc_35 = pgb052,
-    opttax_linc_55 = pgb053,
-    opttax_linc_70 = pgb054,
-    opttax_linc_90 = pgb055,
-    opttax_linc_130 = pgb056,
-    opttax_linc_370 = pgb057,
-    opttax_linc_1800 = pgb058,
-
-    addtax = pgb090, #福祉拡充のための税の追加負担の意向
-    taxact_pg = pgc007, #政府の公共財供給で自身の納税行動が変化する
-    taxact_trust = pgc008, #政府の信頼で自身の納税行動が変化する
-
-    polipref = pga020 #政治的な立ち位置（1:極右 <-> 5:極左）
   ) %>%
   mutate_at(
     vars(
@@ -126,20 +96,10 @@ ses <- raw %>%
       TRUE ~ as.numeric(credit_donate_linc)
     ),
 
-    trust = 6 - trust, #5:very high <-> 1:very low
-
-    welfare = if_else(year < 2015, pea002, pgb020),
-    welfare = case_when(
-      welfare == -9 ~ NA_real_,
-      TRUE ~ 6 - welfare #5:very high <-> 1:very low
-    ),
-
-    taxact_pg = 6 - taxact_pg,
-    taxact_trust = 6 - taxact_trust,
-
-    avg_balance = if_else(avg_balance == -9, NA_real_, avg_balance)
-  ) %>%
-  dplyr::select(-pea002, -pgb020)
+    head = if_else(p_prel == 1, 1, 0),
+    wife = if_else(p_prel == 2, 1, 0),
+    child = if_else(p_prel %in% c(3, 31:37), 1, 0)
+  )
 
 #'
 #' 個人属性の変数ラベルの設定
@@ -159,33 +119,11 @@ ses_label <- list(
   d_relief_donate = "[世帯員]労働・総合所得に対する寄付金控除の有無",
   deduct_donate_linc = "[世帯員]労働所得に対する寄付金所得控除額",
   credit_donate_linc = "[世帯員]労働所得に対する寄付金税額控除額",
-  trust = "[世帯員]政治家への信頼度（2015-2018年調査）",
-  welfare = "[世帯員]納税レベルに対する福祉水準（2009-2018年調査）",
-  avg_balance = "[世帯員]税負担と福祉水準の程度（2015-2018年調査）",
-  opt_balance = "[世帯員]望ましい税負担と福祉水準の程度（2015-2018年調査）",
-  addtax = "[世帯員]福祉拡充の追加課税への選好（2015-2018年調査）",
-  polipref = "[世帯員]政治的選好（left or right）（2015-2018年調査）",
-  opttax_tinc_1 = "[世帯員]年間所得10mKRWの適切な税率（2018年調査）",
-  opttax_tinc_3 = "[世帯員]年間所得30mKRWの適切な税率（2018年調査）",
-  opttax_tinc_5 = "[世帯員]年間所得50mKRWの適切な税率（2018年調査）",
-  opttax_tinc_7 = "[世帯員]年間所得70mKRWの適切な税率（2018年調査）",
-  opttax_tinc_10 = "[世帯員]年間所得100mKRWの適切な税率（2018年調査）",
-  opttax_tinc_20 = "[世帯員]年間所得200mKRWの適切な税率（2018年調査）",
-  opttax_tinc_30 = "[世帯員]年間所得300mKRWの適切な税率（2018年調査）",
-  opttax_tinc_50 = "[世帯員]年間所得500mKRWの適切な税率（2018年調査）",
-  opttax_tinc_100 = "[世帯員]年間所得1000mKRWの適切な税率（2018年調査）",
-  taxact_pg = "[世帯員]政府公共財の申告行動への影響（2016年調査）",
-  taxact_trust = "[世帯員]政府に対する信頼の申告行動への影響（2016年調査）",
-  opttax_linc_15 = "[世帯員]年間労働所得15mKRWの税負担の程度（2016年調査）",
-  opttax_linc_35 = "[世帯員]年間労働所得35mKRWの税負担の程度（2016年調査）",
-  opttax_linc_55 = "[世帯員]年間労働所得55mKRWの税負担の程度（2016年調査）",
-  opttax_linc_70 = "[世帯員]年間労働所得70mKRWの税負担の程度（2016年調査）",
-  opttax_linc_90 = "[世帯員]年間労働所得90mKRWの税負担の程度（2016年調査）",
-  opttax_linc_130 = "[世帯員]年間労働所得130mKRWの税負担の程度（2016年調査）",
-  opttax_linc_370 = "[世帯員]年間労働所得370mKRWの税負担の程度（2016年調査）",
-  opttax_linc_1800 = "[世帯員]年間労働所得1800mKRWの税負担の程度（2016年調査）",
-  taxact_pg = "[世帯員]政府の公共財供給で自身の納税行動が変化するか（2015-2016年調査）",
-  taxact_trust = "[世帯員]政府の信頼で自身の納税行動が変化するか（2015-2016年調査）"
+  head = "[世帯員]世帯主ダミー",
+  wife = "[世帯員]世帯主の配偶者ダミー",
+  child = "[世帯員]世帯主の子供ダミー",
+  position = "[世帯員]従事地位（1=常用、2=臨時、3=自営業、4=無給の家族従事者）",
+  working = "[世帯員]就業状態（1=就職、2=専業主婦、3=無職、4=学生）"
 )
 
 for (i in names(ses_label)) {
@@ -252,16 +190,9 @@ donate_member <- donate_purpose %>%
   mutate(
     donate = donate_religious + donate_welfare + donate_educ + donate_others +
       donate_religious_action + donate_culture + donate_unknown,
-    d_donate = if_else(donate > 0, 1, 0),
-    d_donate_religious = if_else(donate_religious > 0, 1, 0),
-    d_donate_welfare = if_else(donate_welfare > 0, 1, 0),
-    d_donate_educ = if_else(donate_educ > 0, 1, 0),
-    d_donate_others = if_else(donate_others > 0, 1, 0),
-    d_donate_poliparty = if_else(donate_poliparty > 0, 1, 0),
-    d_donate_religious_action = if_else(donate_religious_action > 0, 1, 0),
-    d_donate_culture = if_else(donate_culture > 0, 1, 0),
-    d_donate_unknown = if_else(donate_unknown > 0, 1, 0)
-  )
+    d_donate = if_else(donate > 0, 1, 0)
+  ) %>%
+  dplyr::select(hhid, pid, year, donate, d_donate)
 
 # 寄付支出データ（世帯単位）
 donate_household <- raw %>%
@@ -280,23 +211,15 @@ donate <- donate_member %>%
 #' 寄付データのラベルを設定
 #+
 donate_label <- list(
-  donate_religious = "[世帯員]宗教団体に対する寄付（単位:10,000KRW）",
-  donate_welfare = "[世帯員]社会福祉団体に対する寄付（単位:10,000KRW）",
-  donate_educ = "[世帯員]教育団体に対する寄付（単位:10,000KRW）",
-  donate_poliparty = "[世帯員]政治団体に対する寄付（単位:10,000KRW）",
-  donate_culture = "[世帯員]文化団体に対する寄付（単位:10,000KRW）",
-  donate_religious_action = "[世帯員]宗教団体の貧困救済活動に対する寄付（単位:10,000KRW）",
-  donate_others = "[世帯員]その他の団体に対する寄付（単位:10,000KRW）",
-  donate_unknown = "[世帯員]支出先不明の寄付（単位:10,000KRW）",
+  # donate_religious = "[世帯員]宗教団体に対する寄付（単位:10,000KRW）",
+  # donate_welfare = "[世帯員]社会福祉団体に対する寄付（単位:10,000KRW）",
+  # donate_educ = "[世帯員]教育団体に対する寄付（単位:10,000KRW）",
+  # donate_poliparty = "[世帯員]政治団体に対する寄付（単位:10,000KRW）",
+  # donate_culture = "[世帯員]文化団体に対する寄付（単位:10,000KRW）",
+  # donate_religious_action = "[世帯員]宗教団体の貧困救済活動に対する寄付（単位:10,000KRW）",
+  # donate_others = "[世帯員]その他の団体に対する寄付（単位:10,000KRW）",
+  # donate_unknown = "[世帯員]支出先不明の寄付（単位:10,000KRW）",
   donate = "[世帯員]年間寄付金支出総額（単位:10,000KRW）",
-  d_donate_religious = "[世帯員]宗教団体に対する寄付の有無",
-  d_donate_welfare = "[世帯員]社会福祉団体に対する寄付の有無",
-  d_donate_educ = "[世帯員]教育団体に対する寄付の有無",
-  d_donate_poliparty = "[世帯員]政治団体に対する寄付の有無",
-  d_donate_culture = "[世帯員]文化団体に対する寄付の有無",
-  d_donate_religious_action = "[世帯員]宗教団体の貧困救済活動に対する寄付の有無",
-  d_donate_others = "[世帯員]その他の団体に対する寄付の有無",
-  d_donate_unknown = "[世帯員]支出先不明の寄付の有無",
   d_donate = "[世帯員]寄付金支出の有無",
   h_donate = "[世帯]年間寄付金支出総額（単位:10,000KRW）",
   d_h_donate = "[世帯]寄付金支出の有無"
@@ -439,7 +362,8 @@ attr(dt$year, "label") <- "年度（調査年度の前年）"
 
 dt <- dt %>%
   dplyr::mutate(
-    employee = if_else(workpos == 1, 1, 0),
+    employee = if_else(position == 1, 1, 0),
+    housewife = if_else(working == 2, 1, 0),
     sex = sex - 1,
     univ = case_when(
       educ == 3 ~ 1,
@@ -462,22 +386,6 @@ dt <- dt %>%
       credit_neutral == 1 ~ 2,
       credit_loss == 1 ~ 3,
       TRUE ~ NA_real_
-    ),
-    avg_balance_5scale = case_when(
-      avg_balance == 1 ~ 2,
-      avg_balance %in% c(2, 4) ~ 1,
-      avg_balance %in% c(6, 8) ~ -1,
-      avg_balance == 9 ~ -2,
-      is.na(avg_balance) ~ NA_real_,
-      TRUE ~ 0
-    ),
-    opt_balance_5scale = case_when(
-      opt_balance == 1 ~ 2,
-      opt_balance %in% c(2, 4) ~ 1,
-      opt_balance %in% c(6, 8) ~ -1,
-      opt_balance == 9 ~ -2,
-      is.na(avg_balance) ~ NA_real_,
-      TRUE ~ 0
     ),
     price = case_when(
       year < 2014 ~ 1 - first_mtr,
@@ -515,23 +423,22 @@ dt <- dt %>%
     )
   )
 
+names(dt)
+
 dt <- dt %>%
   dplyr::mutate_at(
     vars(price, lprice, price_iv1, price_iv2, price_iv3),
     list(ln = ~ log(.))
   ) %>%
-  dplyr::mutate_at(
-    vars(starts_with("donate")),
-    list(ln = ~ log(. + 1))
+  dplyr::mutate(
+    linc_ln = log(linc + 100000),
+    donate_ln = log(donate),
+    donate_ln = if_else(is.nan(donate_ln), NA_real_, donate_ln)
   ) %>%
-  dplyr::mutate(linc_ln = log(linc + 100000)) %>%
   dplyr::group_by(pid) %>%
   dplyr::mutate_at(
     vars(
-      price_ln, donate_ln, donate_religious_ln,
-      donate_welfare_ln, donate_educ_ln, donate_poliparty_ln,
-      donate_others_ln, donate_religious_action_ln,
-      donate_culture_ln, donate_unknown_ln, linc_ln,
+      price_ln, donate_ln, linc_ln,
       age, sqage
     ),
     list(
@@ -550,33 +457,6 @@ dt <- dt %>%
     donate_ln_d1 = donate_ln - donate_ln_l1,
     donate_ln_d2 = donate_ln - donate_ln_l2,
     donate_ln_d3 = donate_ln - donate_ln_l3,
-    donate_religious_ln_d1 = donate_religious_ln - donate_religious_ln_l1,
-    donate_religious_ln_d2 = donate_religious_ln - donate_religious_ln_l2,
-    donate_religious_ln_d3 = donate_religious_ln - donate_religious_ln_l3,
-    donate_welfare_ln_d1 = donate_welfare_ln - donate_welfare_ln_l1,
-    donate_welfare_ln_d2 = donate_welfare_ln - donate_welfare_ln_l2,
-    donate_welfare_ln_d3 = donate_welfare_ln - donate_welfare_ln_l3,
-    donate_educ_ln_d1 = donate_educ_ln - donate_educ_ln_l1,
-    donate_educ_ln_d2 = donate_educ_ln - donate_educ_ln_l2,
-    donate_educ_ln_d3 = donate_educ_ln - donate_educ_ln_l3,
-    donate_poliparty_ln_d1 = donate_poliparty_ln - donate_poliparty_ln_l1,
-    donate_poliparty_ln_d2 = donate_poliparty_ln - donate_poliparty_ln_l2,
-    donate_poliparty_ln_d3 = donate_poliparty_ln - donate_poliparty_ln_l3,
-    donate_others_ln_d1 = donate_others_ln - donate_others_ln_l1,
-    donate_others_ln_d2 = donate_others_ln - donate_others_ln_l2,
-    donate_others_ln_d3 = donate_others_ln - donate_others_ln_l3,
-    donate_religious_action_ln_d1 =
-      donate_religious_action_ln - donate_religious_action_ln_l1,
-    donate_religious_action_ln_d2 =
-      donate_religious_action_ln - donate_religious_action_ln_l2,
-    donate_religious_action_ln_d3 =
-      donate_religious_action_ln - donate_religious_action_ln_l3,
-    donate_culture_ln_d1 = donate_culture_ln - donate_culture_ln_l1,
-    donate_culture_ln_d2 = donate_culture_ln - donate_culture_ln_l2,
-    donate_culture_ln_d3 = donate_culture_ln - donate_culture_ln_l3,
-    donate_unknown_ln_d1 = donate_unknown_ln - donate_unknown_ln_l1,
-    donate_unknown_ln_d2 = donate_unknown_ln - donate_unknown_ln_l2,
-    donate_unknown_ln_d3 = donate_unknown_ln - donate_unknown_ln_l3,
     linc_ln_d1 = linc_ln - linc_ln_l1,
     linc_ln_d2 = linc_ln - linc_ln_l2,
     linc_ln_d3 = linc_ln - linc_ln_l3,
@@ -596,13 +476,12 @@ names(dt)
 #+
 cov_label <- list(
   employee = "[世帯員]常用職（給与所得者）ダミー",
+  housewife = "[世帯員]専業主婦ダミー",
   univ = "[世帯員]大卒ダミー",
   highschool = "[世帯員]高卒ダミー",
   junior = "[世帯員]中卒ダミー",
   sqage = "[世帯員]年齢の二乗/100",
   credit_treat = "[世帯員]2014年税制改正の利益",
-  avg_balance_5scale = "[世帯員]現在の税負担と福祉水準のバランス(5段階)",
-  opt_balance_5scale = "[世帯員]理想的な税負担と福祉水準のバランス(5段階)",
   price = "[世帯員]first giving price",
   lprice = "[世帯員]last giving price",
   price_l1_deduct = "[世帯員]first giving price (lag1 income, tax deduction)",
@@ -620,14 +499,6 @@ cov_label <- list(
   price_iv2_ln = "price_iv2の対数値",
   price_iv3_ln = "price_iv3の対数値",
   donate_ln = "donateの対数値",
-  donate_religious_ln = "donate_religiousの対数値",
-  donate_welfare_ln = "donate_welfareの対数値",
-  donate_educ_ln = "donate_educの対数値",
-  donate_poliparty_ln = "donate_polipartyの対数値",
-  donate_others_ln = "donate_othersの対数値",
-  donate_religious_action_ln = "donate_religious_actionの対数値",
-  donate_culture_ln = "donate_cultureの対数値",
-  donate_unknown_ln = "donate_unknownの対数値",
   linc_ln = "lincの対数値",
   price_ln_d1 = "price_ln - price_ln (lag = 1)",
   price_ln_d2 = "price_ln - price_ln (lag = 2)",
@@ -635,39 +506,6 @@ cov_label <- list(
   donate_ln_d1 = "donate_ln - donate_ln (lag = 1)",
   donate_ln_d2 = "donate_ln - donate_ln (lag = 2)",
   donate_ln_d3 = "donate_ln - donate_ln (lag = 3)",
-  donate_religious_ln_d1 =
-    "donate_religious_ln - donate_religious_ln (lag = 1)",
-  donate_religious_ln_d2 =
-    "donate_religious_ln - donate_religious_ln (lag = 2)",
-  donate_religious_ln_d3 =
-    "donate_religious_ln - donate_religious_ln (lag = 3)",
-  donate_welfare_ln_d1 = "donate_welfare_ln - donate_welfare_ln (lag = 1)",
-  donate_welfare_ln_d2 = "donate_welfare_ln - donate_welfare_ln (lag = 2)",
-  donate_welfare_ln_d3 = "donate_welfare_ln - donate_welfare_ln (lag = 3)",
-  donate_educ_ln_d1 = "donate_educ_ln - donate_educ_ln (lag = 1)",
-  donate_educ_ln_d2 = "donate_educ_ln - donate_educ_ln (lag = 2)",
-  donate_educ_ln_d3 = "donate_educ_ln - donate_educ_ln (lag = 3)",
-  donate_poliparty_ln_d1 =
-    "donate_poliparty_ln - donate_poliparty_ln (lag = 1)",
-  donate_poliparty_ln_d2 =
-    "donate_poliparty_ln - donate_poliparty_ln (lag = 2)",
-  donate_poliparty_ln_d3 =
-    "donate_poliparty_ln - donate_poliparty_ln (lag = 3)",
-  donate_others_ln_d1 = "donate_others_ln - donate_others_ln (lag = 1)",
-  donate_others_ln_d2 = "donate_others_ln - donate_others_ln (lag = 2)",
-  donate_others_ln_d3 = "donate_others_ln - donate_others_ln (lag = 3)",
-  donate_religious_action_ln_d1 =
-    "donate_religious_action_ln - donate_religious_action_ln (lag = 1)",
-  donate_religious_action_ln_d2 =
-    "donate_religious_action_ln - donate_religious_action_ln (lag = 2)",
-  donate_religious_action_ln_d3 =
-    "donate_religious_action_ln - donate_religious_action_ln (lag = 3)",
-  donate_culture_ln_d1 = "donate_culture_ln - donate_culture_ln (lag = 1)",
-  donate_culture_ln_d2 = "donate_culture_ln - donate_culture_ln (lag = 2)",
-  donate_culture_ln_d3 = "donate_culture_ln - donate_culture_ln (lag = 3)",
-  donate_unknown_ln_d1 = "donate_unknown_ln - donate_unknown_ln (lag = 1)",
-  donate_unknown_ln_d2 = "donate_unknown_ln - donate_unknown_ln (lag = 2)",
-  donate_unknown_ln_d3 = "donate_unknown_ln - donate_unknown_ln (lag = 3)",
   linc_ln_d1 = "linc_ln - linc_ln (lag = 1)",
   linc_ln_d2 = "linc_ln - linc_ln (lag = 2)",
   linc_ln_d3 = "linc_ln - linc_ln (lag = 3)",
@@ -741,7 +579,7 @@ dt <- dt %>%
 
 #' CSVファイルに書き出す
 #+
-readr::write_csv(dt, file = "data/shaped2.csv")
+readr::write_csv(dt, file = here("data/shaped2.csv"))
 
 #' 変数の記述に関するcsvデータの作成
 #+
@@ -754,4 +592,7 @@ for (i in names(dt)) {
   ))
 }
 
-readr::write_csv(book, file = "data/codebook/shaped2_description.csv")
+readr::write_csv(
+  book,
+  file = here("data/codebook/shaped2_description.csv")
+)
