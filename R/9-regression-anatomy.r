8d66cdc6192ae6a1c2c264302d6392514e19a416
KatoPachi <hkato.econ@gmail.com>
2022-06-29 00:26:56 +0900

    add skeleton and create html preview

--- R/9-regression-anatomy.r
+++ R/9-regression-anatomy.r
@@ -1,11 +1,16 @@
 #' ---
 #' title: |
-#'   Estimating Conventional Price Elasticity of Charitable Giving (1)
-#' author: Hiroki Kato
+#'   Estimating Effect of Tax Incentives on Donations
+#'   Considering Self-Selection of Tax Incentives in South Korea
+#' subtitle: |
+#'   Results of Fixed Effect Model
+#' author:
+#'   - Hiroki Kato
+#'   - Tsuyoshi Goto
+#'   - Yongrok Kim
 #' output:
 #'   bookdown::html_document2:
 #'     toc: true
-#'     toc_float: true
 #'     number_sections: false
 #' params:
 #'   preview: true
@@ -41,6 +46,12 @@ fixest::setFixest_fml(
     year + pid + indust + area
 )
 
+#'
+#' ベースラインとして、操作変数を用いない固定効果モデルの結果を示す
+#'
+#' - 説明変数に用いる価格は、控除を申請するときに適用される価格（applicable price）
+#' と控除の有無による価格変動を考慮した現実の価格（effective price）の二つを用いる
+#'
 #+ anatomy-intensive
 reg_price_int <- use %>%
   dplyr::filter(d_donate == 1) %>%
@@ -84,7 +95,7 @@ title <- c(
   effective = "B. Effective Price"
 )
 
-#+ plot-anatomy-intensive
+#+ plot-anatomy-intensive, fig.cap = "Relationship between Giving Amount and Residuals of Giving Price among Donors."
 plot_int <- names(x_labs) %>%
   purrr::map(function(x) {
     int_use %>%
@@ -133,6 +144,25 @@ plot_int %>%
   theme(legend.position = "bottom")
 
 #'
+#' 図\@ref(fig:plot-anatomy-intensive)は寄付者に限定した寄付額と価格の関係を示している。
+#'
+#' - 縦軸は寄付者の寄付額であり、横軸は価格の残差である。
+#'   - 価格の残差は、価格以外の共変量と固定効果で価格を回帰するモデルから得ている。
+#' - パネルAとBはそれぞれ、applicable priceとeffective priceを用いている。
+#' - どちらのパネルについても、価格の残差がゼロのあたりにサンプルが集中している。
+#' - どちらのパネルについても、控除申請の有無によって、寄付額に大きな差がない。
+#'
+#' Regression anatomy theoremより、
+#' 価格の残差に関する寄付額の単直線回帰分析はintensive-marginの価格弾力性となる。
+#' 図\@ref(fig:plot-anatomy-intensive)の二つのパネルに示した直線はこの回帰直線である。
+#'
+#' - パネルAより、applicable priceを用いたintensive-marginの価格弾力性はおよそ-0.97である。
+#' - パネルBより、effective priceを用いたintensive-marginの価格弾力性はおよそ-0.74である。
+#' - 二つの価格弾力性の標準誤差を考慮すると、二つの価格弾力性の差は誤差の範囲であると考える。
+#' - 二つの価格弾力性が似たような値を取った原因として、
+#' 控除を申請していない人の平均的な寄付額が、価格に関わらず、
+#' 控除を申請している人のそれと大きな差がないことにある。
+#'
 #+ anatomy-extensive
 reg_price_ext <- use %>%
   group_by(price_type) %>%
@@ -163,7 +193,7 @@ reg_anatomy_ext <- ext_use %>%
     )
   )
 
-#+ plot-anatomy-extensive
+#+ plot-anatomy-extensive, fig.cap = "Relationship between Decision to Donate and Residuals of Giving Price."
 plot_ext <- names(x_labs) %>%
   purrr::map(function(x) {
     ext_use %>%
@@ -200,7 +230,7 @@ plot_ext <- names(x_labs) %>%
       labs(
         title = title[x],
         x = x_labs[x],
-        y = "log(donate) conditional on givers",
+        y = "Proportion of Donors",
         shape = "Application of tax relief"
       ) +
       ggtemp()
@@ -211,6 +241,30 @@ plot_ext %>%
   plot_layout(guides = "collect") &
   theme(legend.position = "bottom")
 
+#'
+#' 図\@ref(fig:plot-anatomy-extensive)は寄付行動と寄付価格の残差の関係を示している。
+#'
+#' - 縦軸は寄付者の寄付額であり、横軸は価格の残差である。
+#'   - 価格の残差は、価格以外の共変量と固定効果で価格を回帰するモデルから得ている。
+#' - パネルAとBはそれぞれ、applicable priceとeffective priceを用いている。
+#' - どちらのパネルについても、寄付控除を申請した人は全員寄付をしている。
+#' その一方で、控除を申請していない人の寄付者の割合は半数に満たない。
+#' - Effective priceを用いるとき、控除を申請していない人の価格は常に1となるので、
+#' その残差はapplicable priceの残差よりも正の方向に移動している。
+#'
+#' Regression anatomy theoremより、
+#' 価格の残差に関する寄付行動の単直線回帰分析は固定効果モデルの価格の係数となる。
+#' 図\@ref(fig:plot-anatomy-intensive)の二つのパネルに示した直線はこの回帰直線である。
+#'
+#' - アウトカム変数が二値なので、
+#' この回帰直線の傾きをextensive-marginの価格弾力性として解釈できない。
+#'   - 傾きの係数を寄付者割合で割ることによって、extensive-marginの価格弾力性を得る
+#' - パネルAより、applicable priceを用いるとき、extensive-marginの価格弾力性は
+#' およそ-0.8($=-0.192 / 0.24$)である。
+#' - パネルBより、effective priceを用いるとき、extensive-marginの価格弾力性は
+#' およそ-11.7($=-2.797 / 0.24$)である。
+#' - effective priceを用いることによって、extensive-marginの価格弾力性はより弾力的になった。
+#'
 # /*
 #+
 rmarkdown::render(
