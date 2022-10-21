23c23507ef05f66a6d8278b2992461f5a0755338
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2022-10-21 13:51:55 +0900

    create source file of event-study

--- R/3-fe-model.r
+++ R/3-fe-model.r
@@ -17,10 +17,7 @@ use <- readr::read_csv(here("data/shaped2.csv")) %>%
     have_dependents,
     indust,
     area,
-    credit_benefit,
-    credit_loss,
     price_ln,
-    lprice_ln,
     d_relief_donate,
     employee,
     outcome_intensive = donate_ln,
@@ -38,8 +35,7 @@ use <- readr::read_csv(here("data/shaped2.csv")) %>%
   mutate(
     effective_reverse = (1 - d_relief_donate) * price_ln,
     effective = d_relief_donate * price_ln,
-    applicable = price_ln,
-    after = if_else(year >= 2014, 1, 0)
+    applicable = price_ln
   )
 
 fixest::setFixest_fml(
@@ -425,88 +421,3 @@ subset(est_announce, type == "extensive")$est[[1]] %>%
   writeLines(out.file)
 
 close(out.file)
-
-
-#' //NOTE: Execute event-study
-#+ event-study
-event <- use %>%
-  mutate(
-    year12 = if_else(year == 2013, 1, 0),
-    year14 = if_else(year == 2014, 1, 0),
-    year15 = if_else(year == 2015, 1, 0),
-    year16 = if_else(year == 2016, 1, 0),
-    year17 = if_else(year == 2017, 1, 0)
-  ) %>%
-  group_by(type) %>%
-  nest() %>%
-  mutate(fit = map(data, ~ feols(
-    outcome ~ credit_benefit:year12 + credit_loss:year12 +
-      credit_benefit:year14 + credit_loss:year14 +
-      credit_benefit:year15 + credit_loss:year15 +
-      credit_benefit:year16 + credit_loss:year16 +
-      credit_benefit:year17 + credit_loss:year17 + ..stage2,
-    data = subset(.x, flag = 1),
-    cluster = ~hhid
-  ))) %>%
-  mutate(
-    tidy = map(fit, tidy),
-    t = map(fit, ~ qt(
-      0.025,
-      df = attr(vcov(.x, attr = TRUE), "df.t"),
-      lower.tail = FALSE
-    ))
-  ) %>%
-  select(-data, -fit) %>%
-  unnest(c(tidy, t)) %>%
-  dplyr::filter(str_detect(term, "year"))
-
-plot_event <- event %>%
-  bind_rows(tribble(
-    ~type, ~term, ~ estimate, ~std.error, ~statistic, ~p.value, ~t,
-    "intensive", "credit_benefit:year13", 0, 0, 0, 0, 0,
-    "extensive", "credit_benefit:year13", 0, 0, 0, 0, 0,
-    "intensive", "credit_loss:year13", 0, 0, 0, 0, 0,
-    "extensive", "credit_loss:year13", 0, 0, 0, 0, 0
-  )) %>%
-  mutate(
-    year = case_when(
-      str_detect(term, "year12") ~ 2012,
-      str_detect(term, "year13") ~ 2013,
-      str_detect(term, "year14") ~ 2014,
-      str_detect(term, "year15") ~ 2015,
-      str_detect(term, "year16") ~ 2016,
-      str_detect(term, "year17") ~ 2017
-    ),
-    treat = case_when(
-      str_detect(term, "benefit") ~ "Decrease",
-      str_detect(term, "loss") ~ "Increase"
-    ),
-    treat = factor(
-      treat,
-      levels = c("Decrease", "Increase"),
-      labels = c("Decreasing price group", "Increasing price group")
-    ),
-    type = factor(
-      type,
-      levels = c("intensive", "extensive"),
-      labels = c("Intensive-margin", "Extensive-margin")
-    ),
-    ci.low = estimate - std.error * t,
-    ci.high = estimate + std.error * t
-  ) %>%
-  ungroup()
-
-plot_event %>%
-  ggplot(aes(x = year, y = estimate)) +
-  geom_hline(aes(yintercept = 0), linetype = 2) +
-  geom_line(size = 1) +
-  geom_ribbon(aes(ymin = ci.low, ymax = ci.high), alpha = 0.1) +
-  geom_point(size = 4) +
-  facet_wrap(~ type * treat, scale = "free_y") +
-  labs(x = "Year", y = "Estimates (95%CI)") +
-  ggtemp()
-
-ggsave(
-  here("export", "figures", "event-study.pdf"),
-  width = 10, height = 8
-)
