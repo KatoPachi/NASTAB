898855e163805278ac761015fa382ee4061db76b
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2022-10-21 13:46:46 +0900

    add announcement effect

--- R/3-fe-model.r
+++ R/3-fe-model.r
@@ -132,7 +132,7 @@ out.file <- file(here("export", "tables", "main-int.tex"), open = "w")
 subset(est_femod, type == "intensive")$est[[1]] %>%
   setNames(paste0("(", seq(length(.)), ")")) %>%
   modelsummary(
-    title = "Estimation Results of Intenstive-Margin Price Elasticities\\label{tab:main-int}",
+    title = "Estimation Results of Intensive-Margin Price Elasticities\\label{tab:main-int}",
     coef_map = c(
       "applicable" = "Applicable price",
       "effective" = "Effective price",
@@ -222,7 +222,7 @@ out.file <- file(here("export", "tables", "main-ext.tex"), open = "w")
 subset(est_femod, type == "extensive")$est[[1]] %>%
   setNames(paste0("(", seq(length(.)), ")")) %>%
   modelsummary(
-    title = "Estimation Results of Extenstive-Margin Price Elasticities\\label{tab:main-ext}",
+    title = "Estimation Results of Extensive-Margin Price Elasticities\\label{tab:main-ext}",
     coef_map = c(
       "applicable" = "Applicable price",
       "effective" = "Effective price",
@@ -249,6 +249,183 @@ subset(est_femod, type == "extensive")$est[[1]] %>%
   writeLines(out.file)
 
 close(out.file)
+
+#' //NOTE: Estimate price elasticity excluding announcement effect
+#+
+est_announce <- use %>%
+  dplyr::filter(year < 2013 | 2014 < year) %>%
+  mutate(type = factor(type, levels = c("intensive", "extensive"))) %>%
+  group_by(type) %>%
+  do(est = lapply(
+    femod,
+    function(x) feols(x, data = subset(., flag == 1), cluster = ~hhid)
+  ))
+
+#' //NOTE: Create regression table of intensive-margin price elasticity
+#+
+ivtable1 <- tibble(
+  terms = c(
+    "F-statistics of instruments",
+    "Wu-Hausman test, p-value",
+    "Sargan Test, p-value"
+  ),
+  model1 = rep(NA_real_, 3),
+  model2 = rep(NA_real_, 3)
+)
+
+ivtable <- subset(est_announce, type == "intensive")$est[[1]][3:4] %>%
+  purrr::map(function(x) {
+    sargan <- fitstat(x, "sargan")$sargan
+    data.frame(stat = c(
+      fitstat(x, "ivf")[[1]]$stat,
+      fitstat(x, "wh")$wh$p,
+      ifelse(sum(is.na(sargan)) == 0, sargan$p, NA_real_)
+    ))
+  }) %>%
+  reduce(bind_cols) %>%
+  bind_cols(ivtable1, .) %>%
+  mutate_at(
+    vars(-terms), list(~ ifelse(is.na(.), "", sprintf("\\num{%1.3f}", .)))
+  ) %>%
+  mutate_at(
+    vars(-terms), list(~ ifelse(. == "\\num{0.000}", "$<$ \\num{0.001}", .))
+  ) %>%
+  bind_rows(c(
+    "terms" = "Instruments",
+    "model1" = "",
+    "model2" = "",
+    "stat...4" = "Applicable",
+    "stat...5" = "Applicable * Wage earner"
+  ), .)
+
+attr(ivtable, "position") <- 7:10
+
+out.file <- file(here("export", "tables", "announcement-int.tex"), open = "w")
+
+subset(est_announce, type == "intensive")$est[[1]] %>%
+  setNames(paste0("(", seq(length(.)), ")")) %>%
+  modelsummary(
+    title = "Estimation of Intensive-Margin Price Elasticities Excluding Announcement Effect\\label{tab:announce-int}",
+    coef_map = c(
+      "applicable" = "Applicable price",
+      "effective" = "Effective price",
+      "fit_effective" = "Effective price",
+      "tinc_ln" = "Log income"
+    ),
+    gof_omit = "R2 Pseudo|R2 Within|AIC|BIC|Log|Std|FE|R2",
+    stars = c("***" = 0.01, "**" = 0.05, "*" = 0.1),
+    add_rows = ivtable,
+    output = "latex"
+  ) %>%
+  kable_styling(font_size = 8) %>%
+  add_header_above(c(" " = 1, "FE" = 2, "FE-2SLS" = 2)) %>%
+  add_header_above(c(" " = 1, "Log donation" = 4)) %>%
+  group_rows("1st stage information", 7, 10, bold = FALSE, italic = TRUE) %>%
+  column_spec(2:5, width = "7.5em") %>%
+  footnote(
+    general_title = "",
+    general = "Notes: * p < 0.1, ** p < 0.05, *** p < 0.01. We use standard errors clustered at household level. An outcome variable is logged value of amount of charitable giving. For estimation, we use donors only (intensive-margin sample), but exclude samples from 2013 and 2014. For outcome equation, we control squared age (divided by 100), number of household members, a dummy that indicates having dependents, employee dummy, a set of dummies of industry a set of dummies of residential area, and individual and time fixed effects. For FE-2SLS, we use following instrumental variables: $\\\\text{Applicable price}$ in model (3); $\\\\text{Applicable price} + \\\\text{Applicable price} \\\\times \\\\text{Wage earner}$ in model (4).",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) %>%
+  writeLines(out.file)
+
+close(out.file)
+
+#' //NOTE: Create regression table of extensive-margin price elasticity
+#+
+mu <- use %>%
+  dplyr::filter(type == "extensive") %>%
+  dplyr::filter(year < 2013 | 2014 < year) %>%
+  with(mean(outcome))
+
+implied_e <- subset(est_announce, type == "extensive")$est[[1]] %>%
+  purrr::map(function(x) {
+    res <- subset(tidy(x), str_detect(term, "effective|applicable")) %>%
+      mutate(
+        estimate = estimate / mu,
+        estimate = case_when(
+          p.value < 0.01 ~ sprintf("\\num{%1.3f}***", estimate),
+          p.value < 0.05 ~ sprintf("\\num{%1.3f}**", estimate),
+          p.value < 0.1 ~ sprintf("\\num{%1.3f}*", estimate),
+          TRUE ~ sprintf("\\num{%1.3f}", estimate)
+        ),
+        std.error = sprintf("(\\num{%1.3f})", std.error / mu)
+      )
+
+    tribble(
+      ~term, ~mod,
+      "Estimates", res$estimate,
+      "Estimates se", res$std.error
+    )
+  }) %>%
+  reduce(left_join, by = "term") %>%
+  setNames(c("term", paste0("mod", seq(length(femod)))))
+
+ivtable <- subset(est_announce, type == "extensive")$est[[1]][3:4] %>%
+  purrr::map(function(x) {
+    sargan <- fitstat(x, "sargan")$sargan
+    data.frame(stat = c(
+      fitstat(x, "ivf")[[1]]$stat,
+      fitstat(x, "wh")$wh$p,
+      ifelse(sum(is.na(sargan)) == 0, sargan$p, NA_real_)
+    ))
+  }) %>%
+  reduce(bind_cols) %>%
+  bind_cols(ivtable1, .) %>%
+  mutate_at(
+    vars(-terms), list(~ ifelse(is.na(.), "", sprintf("\\num{%1.3f}", .)))
+  ) %>%
+  mutate_at(
+    vars(-terms), list(~ ifelse(. == "\\num{0.000}", "$<$ \\num{0.001}", .))
+  ) %>%
+  bind_rows(c(
+    "terms" = "Instruments",
+    "model1" = "",
+    "model2" = "",
+    "stat...4" = "Applicable",
+    "stat...5" = "Applicable $\\times$ Wage earner"
+  ), .) %>%
+  setNames(c("term", paste0("mod", seq(length(femod)))))
+
+add_table <- bind_rows(implied_e, ivtable) %>%
+  mutate(term = dplyr::recode(term, "Estimates se" = "", .default = term))
+
+attr(add_table, "position") <- 7:12
+
+out.file <- file(here("export", "tables", "announcement-ext.tex"), open = "w")
+
+subset(est_announce, type == "extensive")$est[[1]] %>%
+  setNames(paste0("(", seq(length(.)), ")")) %>%
+  modelsummary(
+    title = "Estimation of Extensive-Margin Price Elasticities Excluding Announcement Effect\\label{tab:announce-ext}",
+    coef_map = c(
+      "applicable" = "Applicable price",
+      "effective" = "Effective price",
+      "fit_effective" = "Effective price",
+      "tinc_ln" = "Log income"
+    ),
+    gof_omit = "R2 Pseudo|R2 Within|AIC|BIC|Log|Std|FE|R2",
+    stars = c("***" = 0.01, "**" = 0.05, "*" = 0.1),
+    add_rows = add_table,
+    output = "latex"
+  ) %>%
+  kable_styling(font_size = 8) %>%
+  add_header_above(c(" " = 1, "FE" = 2, "FE-2SLS" = 2)) %>%
+  add_header_above(c(" " = 1, "A dummy of donor" = 4)) %>%
+  group_rows("Implied price elasticity", 7, 8, italic = TRUE, bold = FALSE) %>%
+  group_rows("1st stage information", 9, 12, italic = TRUE, bold = FALSE) %>%
+  column_spec(2:5, width = "7.5em") %>%
+  footnote(
+    general_title = "",
+    general = "Notes: * p < 0.1, ** p < 0.05, *** p < 0.01. We use standard errors clustered at household level. An outcome variable is a dummy indicating that donor. For estimation, we use not only donors but also non-donors (extensive-margin sample), but exclude samples from 2013 and 2014. For outcome equation, we control squared age (divided by 100), number of household members, a dummy that indicates having dependents, a employee dummy, a set of dummies of industry, a set of dummies of residential area, and individual and time fixed effects. For FE-2SLS, we use following instrumental variables: $\\\\text{Applicable price}$ in model (3); $\\\\text{Applicable price} + \\\\text{Applicable price} \\\\times \\\\text{Wage earner}$ in model (4). We calculate implied price elasticities by dividing estimates by proportion of donors in our sample.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) %>%
+  writeLines(out.file)
+
+close(out.file)
+
 
 #' //NOTE: Execute event-study
 #+ event-study
