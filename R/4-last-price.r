fa96ae80f8841ea4fe070972c2c2e41c9d5f42a1
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2022-11-08 19:19:37 +0900

    fix dataset: 4-last-price

--- R/4-last-price.r
+++ R/4-last-price.r
@@ -4,13 +4,19 @@ source(here("R", "_library.r"))
 
 #+ include = FALSE
 use <- readr::read_csv(here("data/shaped2.csv")) %>%
-  dplyr::filter(year < 2018) %>%
+  dplyr::filter(2010 <= year & year < 2018) %>%
+  dplyr::filter(tinc < 1100 | 1300 < tinc) %>%
+  dplyr::filter(tinc < 4500 | 4700 < tinc) %>%
+  dplyr::filter(tinc < 8700 | 8900 < tinc) %>%
+  dplyr::filter(tinc < 14000 | 16000 < tinc) %>%
+  dplyr::filter(tinc < 30000) %>%
   dplyr::filter(dependents == 0) %>%
   dplyr::filter(tinc > donate) %>%
   select(
     pid,
     hhid,
     year,
+    tinc,
     tinc_ln,
     sqage,
     hh_num,
@@ -40,43 +46,6 @@ use <- readr::read_csv(here("data/shaped2.csv")) %>%
     applicable_last = lprice_ln
   )
 
-#' //NOTE: Relationship b/w Applicable and Effective
-#+ plot-stage1, fig.cap = "Relationship between Applicable First Price and Last Price by Employment Status. Note: The bubble size indicates sample size. Due to the small sample size, the leftmost bubbles for salaried and self-employed workers are less informative ($N=6$ for wage earner and $N=2$ for self-employed).", out.extra = ""
-plot_stage1 <- use %>%
-  dplyr::filter(type == "extensive") %>%
-  dplyr::filter(
-    !is.na(d_relief_donate) & !is.na(lprice_ln) & !is.na(employee)
-  ) %>%
-  mutate(
-    employee = factor(employee, label = c("Others", "Wage earners"))
-  ) %>%
-  group_by(applicable, employee) %>%
-  summarize(
-    n = n(),
-    applicable.last = mean(lprice_ln)
-  ) %>%
-  ggplot(aes(x = applicable, y = applicable.last)) +
-  geom_abline(aes(intercept = 0, slope = 1), linetype = 2) +
-  geom_point(aes(size = n, color = employee), alpha = 0.8) +
-  scale_color_grey() +
-  scale_size(range = c(5, 20)) +
-  labs(
-    x = "log(first price)",
-    y = "Average of log(last price)",
-    color = ""
-  ) +
-  guides(
-    color = guide_legend(override.aes = list(size = 5)),
-    size = "none"
-  ) +
-  ggtemp(size = list(title = 15, text = 13, caption = 13))
-
-ggsave(
-  here("export", "figures", "plot-first-last-price.pdf"),
-  width = 10,
-  height = 5
-)
-
 #' //NOTE: Estimate last-price elasticity
 #+ reg-stage1, eval = FALSE
 fixest::setFixest_fml(
@@ -88,8 +57,7 @@ femod <- list(
   outcome ~ applicable_last + ..stage2,
   outcome ~ effective_last + ..stage2,
   outcome ~ ..stage2 | applicable_last ~ applicable,
-  outcome ~ ..stage2 | effective_last ~ applicable,
-  outcome ~ ..stage2 | effective_last ~ applicable + applicable:employee
+  outcome ~ ..stage2 | effective_last ~ applicable
 )
 
 est_femod <- use %>%
@@ -105,20 +73,18 @@ est_femod <- use %>%
 ivtable1 <- tibble(
   terms = c(
     "F-statistics of instruments",
-    "Wu-Hausman test, p-value",
-    "Sargan Test, p-value"
+    "Wu-Hausman test, p-value"
   ),
-  model1 = rep(NA_real_, 3),
-  model2 = rep(NA_real_, 3)
+  model1 = rep(NA_real_, 2),
+  model2 = rep(NA_real_, 2)
 )
 
-ivtable <- subset(est_femod, type == "intensive")$est[[1]][3:5] %>%
+ivtable <- subset(est_femod, type == "intensive")$est[[1]][3:4] %>%
   purrr::map(function(x) {
     sargan <- fitstat(x, "sargan")$sargan
     data.frame(stat = c(
       fitstat(x, "ivf")[[1]]$stat,
-      fitstat(x, "wh")$wh$p,
-      ifelse(sum(is.na(sargan)) == 0, sargan$p, NA_real_)
+      fitstat(x, "wh")$wh$p
     ))
   }) %>%
   reduce(bind_cols) %>%
@@ -128,15 +94,7 @@ ivtable <- subset(est_femod, type == "intensive")$est[[1]][3:5] %>%
   ) %>%
   mutate_at(
     vars(-terms), list(~ ifelse(. == "\\num{0.000}", "$<$ \\num{0.001}", .))
-  ) %>%
-  bind_rows(c(
-    "terms" = "Instruments",
-    "model1" = "",
-    "model2" = "",
-    "stat...4" = "Applicable",
-    "stat...5" = "Applicable",
-    "stat...6" = "Applicable $\\times$ Wage earner"
-  ), .)
+  )
 
 attr(ivtable, "position") <- 7:10
 
@@ -159,13 +117,16 @@ subset(est_femod, type == "intensive")$est[[1]] %>%
     output = "latex"
   ) %>%
   kable_styling(font_size = 8) %>%
-  add_header_above(c(" " = 1, "FE" = 2, "FE-2SLS" = 3)) %>%
-  add_header_above(c(" " = 1, "Log donation" = 5)) %>%
-  group_rows("1st stage information", 7, 10, bold = FALSE, italic = TRUE) %>%
-  column_spec(2:6, width = "6em") %>%
+  add_header_above(c(" " = 1, "FE" = 2, "FE-2SLS" = 2)) %>%
+  add_header_above(c(" " = 1, "Log donation" = 4)) %>%
+  group_rows(
+    "1st stage information (Excluded instrument: Applicable price)",
+    7, 8, bold = FALSE, italic = TRUE
+  ) %>%
+  column_spec(2:5, width = "6.25em") %>%
   footnote(
     general_title = "",
-    general = "Notes: * p < 0.1, ** p < 0.05, *** p < 0.01. We use standard errors clustered at household level. An outcome variable is logged value of amount of charitable giving. For estimation, we use donors only (intensive-margin sample). For outcome equation, we control squared age (divided by 100), number of household members, a dummy that indicates having dependents, employee dummy, a set of dummies of industry a set of dummies of residential area, and individual and time fixed effects. For FE-2SLS, we use following instrumental variables: $\\\\text{Applicable first-price}$ in model (3) and (4); $\\\\text{Applicable first-price} + \\\\text{Applicable first-price} \\\\times \\\\text{Wage earner}$ in model (5).",
+    general = "Notes: * p < 0.1, ** p < 0.05, *** p < 0.01. Standard errors clustered at household level are in parenthesis. An outcome variable is logged value of amount of charitable giving. For estimation, we use donors only (intensive-margin sample). For outcome equation, we control squared age (divided by 100), number of household members, a dummy that indicates having dependents, employee dummy, a set of dummies of industry a set of dummies of residential area, and individual and time fixed effects. For FE-2SLS, we use logged applicable price as an instrument.",
     threeparttable = TRUE,
     escape = FALSE
   ) %>%
@@ -200,13 +161,12 @@ implied_e <- subset(est_femod, type == "extensive")$est[[1]] %>%
   reduce(left_join, by = "term") %>%
   setNames(c("term", paste0("mod", seq(length(femod)))))
 
-ivtable <- subset(est_femod, type == "extensive")$est[[1]][3:5] %>%
+ivtable <- subset(est_femod, type == "extensive")$est[[1]][3:4] %>%
   purrr::map(function(x) {
     sargan <- fitstat(x, "sargan")$sargan
     data.frame(stat = c(
       fitstat(x, "ivf")[[1]]$stat,
-      fitstat(x, "wh")$wh$p,
-      ifelse(sum(is.na(sargan)) == 0, sargan$p, NA_real_)
+      fitstat(x, "wh")$wh$p
     ))
   }) %>%
   reduce(bind_cols) %>%
@@ -217,20 +177,12 @@ ivtable <- subset(est_femod, type == "extensive")$est[[1]][3:5] %>%
   mutate_at(
     vars(-terms), list(~ ifelse(. == "\\num{0.000}", "$<$ \\num{0.001}", .))
   ) %>%
-  bind_rows(c(
-    "terms" = "Instruments",
-    "model1" = "",
-    "model2" = "",
-    "stat...4" = "Applicable",
-    "stat...5" = "Applicable",
-    "stat...6" = "Applicable $\\times$ Wage earner"
-  ), .) %>%
   setNames(c("term", paste0("mod", seq(length(femod)))))
 
 add_table <- bind_rows(implied_e, ivtable) %>%
   mutate(term = dplyr::recode(term, "Estimates se" = "", .default = term))
 
-attr(add_table, "position") <- 7:12
+attr(add_table, "position") <- 7:10
 
 out.file <- file(here("export", "tables", "last-ext.tex"), open = "w")
 
@@ -251,14 +203,17 @@ subset(est_femod, type == "extensive")$est[[1]] %>%
     output = "latex"
   ) %>%
   kable_styling(font_size = 8) %>%
-  add_header_above(c(" " = 1, "FE" = 2, "FE-2SLS" = 3)) %>%
-  add_header_above(c(" " = 1, "A dummy of donor" = 5)) %>%
+  add_header_above(c(" " = 1, "FE" = 2, "FE-2SLS" = 2)) %>%
+  add_header_above(c(" " = 1, "A dummy of donor" = 4)) %>%
   group_rows("Implied price elasticity", 7, 8, italic = TRUE, bold = FALSE) %>%
-  group_rows("1st stage information", 9, 12, italic = TRUE, bold = FALSE) %>%
-  column_spec(2:6, width = "6em") %>%
+  group_rows(
+    "1st stage information (Excluded instrument: Applicable price)",
+    9, 10, italic = TRUE, bold = FALSE
+  ) %>%
+  column_spec(2:5, width = "6.25em") %>%
   footnote(
     general_title = "",
-    general = "Notes: * p < 0.1, ** p < 0.05, *** p < 0.01. We use standard errors clustered at household level. An outcome variable is a dummy indicating that donor. For estimation, we use not only donors but also non-donors (extensive-margin sample). For outcome equation, we control squared age (divided by 100), number of household members, a dummy that indicates having dependents, a employee dummy, a set of dummies of industry, a set of dummies of residential area, and individual and time fixed effects. For FE-2SLS, we use following instrumental variables: $\\\\text{Applicable first-price}$ in model (3) and (4); $\\\\text{Applicable first-price} + \\\\text{Applicable first-price} \\\\times \\\\text{Wage earner}$ in model (5). We calculate implied price elasticities by dividing estimates by proportion of donors in our sample.",
+    general = "Notes: * p < 0.1, ** p < 0.05, *** p < 0.01. Standard errors clustered at household level are in parentheses. An outcome variable is a dummy indicating that donor. For estimation, we use not only donors but also non-donors (extensive-margin sample). For outcome equation, we control squared age (divided by 100), number of household members, a dummy that indicates having dependents, a employee dummy, a set of dummies of industry, a set of dummies of residential area, and individual and time fixed effects. For FE-2SLS, we use the logged applicable price as an instrument. We calculate implied price elasticities by dividing estimates by proportion of donors in our sample.",
     threeparttable = TRUE,
     escape = FALSE
   ) %>%
