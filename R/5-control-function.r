3aec8cc23a5d9996efb9477e16bc836fdd1e55f1
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2023-01-19 11:23:52 +0900

    FIX control function method

--- R/5-control-function.r
+++ R/5-control-function.r
@@ -123,56 +123,69 @@ close(out.file)
 #+
 cf_use <- mundlak_use %>%
   modelr::add_residuals(est_psmod) %>%
-  # modelr::add_predictions(est_psmod, type = "link") %>%
-  # mutate(
-  #   imr = dnorm(pred) / pnorm(pred),
-  #   inv_imr = dnorm(-pred) / pnorm(-pred),
-  #   gr = d_relief_donate * imr - (1 - d_relief_donate) * inv_imr
-  # ) %>%
   pivot_longer(
     outcome_intensive:flag_intensive,
     names_to = c(".value", "type"),
     names_pattern = "(.*)_(.*)"
   )
 
-cfmod <- outcome ~ applicable:d_relief_donate + resid + resid:d_relief_donate +
-  ..mundlak + ..stage2
+cfmod <- list(
+  outcome ~ applicable:d_relief_donate + resid +
+    ..mundlak + ..stage2,
+  outcome ~ applicable:d_relief_donate + resid +
+    resid:applicable:d_relief_donate + ..mundlak + ..stage2
+)
 
 est_cfmod <- cf_use %>%
   mutate(type = factor(type, levels = c("intensive", "extensive"))) %>%
   group_by(type) %>%
-  do(est = lm_robust(xpd(cfmod), data = subset(., flag == 1)))
+  nest() %>%
+  mutate(
+    est1 = map(data, ~ lm_robust(
+      xpd(cfmod[[1]]),
+      data = subset(.x, flag == 1),
+      cluster = hhid, se_type = "stata"
+    )),
+    est2 = map(data, ~ lm_robust(
+      xpd(cfmod[[2]]),
+      data = subset(.x, flag == 1),
+      cluster = hhid, se_type = "stata"
+    ))
+  ) %>%
+  pivot_longer(est1:est2, values_to = "fit", names_to = "model")
 
 #' //NOTE: Create regression tables of CF models
 #+
 mu <- with(subset(cf_use, type == "extensive"), mean(outcome))
 
-implied_e <- subset(est_cfmod, type == "extensive")$est[[1]] %>% {
-  res <- subset(tidy(.), str_detect(term, "applicable:d_relief_donate")) %>%
-    mutate(
-      estimate = estimate / mu,
-      estimate = case_when(
-        p.value < 0.01 ~ sprintf("\\num{%1.3f}***", estimate),
-        p.value < 0.05 ~ sprintf("\\num{%1.3f}**", estimate),
-        p.value < 0.1  ~ sprintf("\\num{%1.3f}*", estimate),
-        TRUE           ~ sprintf("\\num{%1.3f}", estimate)
-      ),
-      std.error = sprintf("(\\num{%1.3f})", std.error / mu)
+implied_e <- est_cfmod %>%
+  mutate(
+    tidy = map(fit, tidy),
+    tidy = map(tidy, ~subset(., str_detect(term, "applicable"))),
+    tidy = map(tidy, ~subset(., !str_detect(term, "resid|mean"))),
+    std.err = map_dbl(tidy, ~ .$std.error / mu),
+    std.err = sprintf("(\\num{%1.3f})", std.err),
+    estimate = map_dbl(tidy, ~ .$estimate / mu),
+    p.value = map_dbl(tidy, ~ .$p.value),
+    estimate = case_when(
+      p.value < 0.01 ~ sprintf("\\num{%1.3f}***", estimate),
+      p.value < 0.05 ~ sprintf("\\num{%1.3f}**", estimate),
+      p.value < 0.1  ~ sprintf("\\num{%1.3f}*", estimate),
+      TRUE           ~ sprintf("\\num{%1.3f}", estimate)
     )
-
-  tribble(
-    ~term, ~mod1, ~mod2,
-    "Estimates", "", res$estimate,
-    "", "", res$std.error
-  )
-}
+  ) %>% {
+    tribble(
+      ~term, ~int_mod1, ~int_mod2, ~ext_mod1, ~ext_mod2,
+      "Estimate", "", "", .$estimate[3], .$estimate[4],
+      "", "", "", .$std.err[3], .$std.err[4]
+    )
+  }
 
 attr(implied_e, "position") <- c(9, 10)
 
 out.file <- file(here("export", "tables", "cf.tex"), open = "w")
 
-est_cfmod$est %>%
-  flatten() %>%
+est_cfmod$fit %>%
   setNames(paste0("(", seq(length(.)), ")")) %>%
   modelsummary(
     title = "Estimation Results of Control Function Model\\label{tab:cf}",
@@ -180,8 +193,8 @@ est_cfmod$est %>%
       "applicable:d_relief_donate" = "Effective price",
       "tinc_ln" = "Log income",
       "resid" = "Residuals of Application",
-      "d_relief_donate:resid" =
-        "Application $\\times$ Residuals of Application"
+      "applicable:d_relief_donate:resid" =
+        "Effective price $\\times$ Residuals of Application"
     ),
     gof_omit = "^(?!R2 Adj.|Num)",
     stars = c("***" = 0.01, "**" = 0.05, "*" = 0.1),
@@ -191,7 +204,7 @@ est_cfmod$est %>%
   ) %>%
   kable_styling(font_size = 8) %>%
   add_header_above(
-    c(" " = 1, "Log donation" = 1, "A dummy of donor" = 1)
+    c(" " = 1, "Log donation" = 2, "A dummy of donor" = 2)
   ) %>%
   group_rows(
     "Implied price elasticity", 9, 10, italic = TRUE, bold = FALSE
