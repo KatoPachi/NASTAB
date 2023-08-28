3bbf2d3d529ffdcde2b2acf8b6d85c99064f6ae5
Hiroki Kato <hkato.econ@gmail.com>
2023-08-28 14:58:22 +0900

    fix output

--- R/output.r
+++ R/output.r
@@ -8,35 +8,60 @@ use <- StartAnalysis$new(here("data/shaped2.csv"))
 # Output options
 options(modelsummary_stars_note = FALSE)
 
-# //NOTE: Table 2: Descriptive Statistics
+# //NOTE: Descriptive statistics
+# Table 2: Descriptive Statistics
 data_summary <- use$summary()
 data_summary$stats()
 
-# //NOTE: Figure 1: Income Distribution in 2013 and Relative Giving Price
+# Figure 1: Income Distribution in 2013 and Relative Giving Price
 data_summary$income_dist()
 
-# //NOTE: Figure 2: Average Giving Amount and Proportion of Donors
+# Figure 2: Average Giving Amount and Proportion of Donors
 data_summary$ts_giving()
 
-# //NOTE: Figure 3: Proportion Having Applied for Tax Incentives
+# Figure 3: Proportion Having Applied for Tax Incentives
 data_summary$ts_claim()
 
-# //NOTE: Appendix ? (reproduce Figure 3 using a subset consisting donors)
+# Appendix ? (reproduce Figure 3 using a subset consisting donors)
 data_summary$ts_claim(d_donate == 1)
 
-# //NOTE: Table 3: Estimation Results of Price Elasticities
+# //NOTE Main results
+# Table A1: First-Stage Models
 est_fp <- use$first_price()
-est_fp$stage2()
-
-# //NOTE: Table A1: First-Stage Models
 est_fp$stage1()
 
-# //NOTE: Table A2: Estimation of Price Elasticities Excluding Announcement Effect
+# Table 3: Estimation Results of Price Elasticities
+est_fp$stage2()
+
+# //NOTE Robustness (Announcement effect)
+# Table A2: Estimation of Price Elasticities Excluding Announcement Effect
 est_fp$exclude_announcement()
 
-# //NOTE: Table A3: Estimation Results of Intensive-Margin Last-Price Elasticities
-est_lp <- use$last_price()$fit()
+# //NOTE Robustness (Last-Price Elasticity)
+# Table A3: Estimation Results of Intensive-Margin Last-Price Elasticities
+lp <- use$last_price()
+est_lp <- lp$fit()
 est_lp$intensive()
 
-# //NOTE: Table A4: Estimation Results of Extensive-Margin Last-Price Elasticities
+# Table A4: Estimation Results of Extensive-Margin Last-Price Elasticities
 est_lp$extensive()
+
+# //NOTE Robustness (Bracket-shifting and permanent income problem?)
+# Appendix ? (Estimation Results of First-Price Elasticities Removing Bracket Shifters)
+use2 <- use$clone()
+use2$remove_bracket_shift()
+use2$first_price()$stage2()
+
+# Appendix ? (Estimation Results of Intensive-Margin Last-Price Elasticities Removing Bracket Shifters)
+lp2_fit <- use2$last_price()$fit()
+lp2_fit$intensive()
+
+# Appendix ? (Estimation Results of Extensive-Margin Last-Price Elasticities Removing Bracket Shifters)
+lp2_fit$extensive()
+
+# //NOTE Discussion
+# Appendix ? (Intensive-Margin First-Price Elasticities among Claimants)
+est_fp$claimant_only()
+
+# Appendix ? (Price Elasticity of Claiming)
+est_fp$claim_elasticity()
