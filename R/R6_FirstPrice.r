e589221f5a8314e3945ed8a71501773b1554c880
Hiroki Kato <hkato.econ@gmail.com>
2024-06-05 16:28:08 +0900

    Fix stage 1 and 2 method in FirstPrice class

--- R/R6_FirstPrice.r
+++ R/R6_FirstPrice.r
@@ -10,65 +10,44 @@ FirstPrice <- R6::R6Class("FirstPrice",
   public = list(
     data = NULL,
     initialize = function(data) {
+      ymin <- with(subset(data, donate > 0), min(donate))
+
       self$data <- data %>%
-        relocate(donate_ln, d_donate, .after = last_col()) %>%
-        rename(
-          outcome_intensive = donate_ln,
-          outcome_extensive = d_donate,
-          applicable = price_ln
-        ) %>%
         mutate(
-          flag_extensive = 1,
-          flag_intensive = if_else(outcome_extensive == 1, 1, 0)
-        ) %>%
-        pivot_longer(
-          outcome_intensive:flag_intensive,
-          names_to = c(".value", "type"),
-          names_pattern = "(.*)_(.*)"
-        ) %>%
-        mutate(
+          donate = donate / ymin,
+          donate_ln = if_else(d_donate == 0, -1, log(donate)),
+          applicable = price_ln,
           effective = d_relief_donate * applicable
-        ) %>%
-        dplyr::filter(flag == 1)
+        )
 
       setFixest_fml(
-        ..stage2 = ~ after_tax_tinc_ln + sqage + hhnum + hhnum_child + dependent_num +
-          hh_max_inc + I(family_position == 1) + employee +
+        ..stage2 = ~ after_tax_tinc_ln + sqage +
+          hhnum + hhnum_child + dependent_num +
+          hh_max_inc + I(family_position == 1) +
           factor(indust) + factor(area) | pid + year
       )
     },
     stage1 = function(title = "", label = "", notes = "", font_size = 8) {
-      est <- self$data %>%
-        group_by(type) %>%
-        nest() %>%
-        mutate(fit = map(data, ~ feols(private$stage1_mod, data = ., cluster = ~hhid))) %>%
-        arrange(desc(type))
-      
+      est <- feols(private$stage1_mod, data = self$data, cluster = ~hhid)
+
       if (label != "") label <- paste0("\\label{tab:", label, "}")
 
-      est %>%
-        pull(fit) %>%
-        setNames(paste0("(", seq(length(.)), ")")) %>%
+      list("(1)" = est) %>%
         modelsummary(
           title = paste0(title, label),
           coef_map = c(
             "applicable" = "Applicable price",
             "after_tax_tinc_ln" = "Log after-tax income"
           ),
-          gof_omit = "R2 Pseudo|R2 Within|AIC|BIC|Log|Std|FE|R2",
+          gof_omit = "R2 Pseudo|R2 Within|AIC|BIC|Log|Std|FE|R2|RMSE",
           stars = c("***" = .01, "**" = .05, "*" = .1),
           escape = FALSE
         ) %>%
         kable_styling(font_size = font_size) %>%
-        add_header_above(c(
-          " " = 1,
-          "Donors (Intensive-margin)" = 1,
-          "Donors and Non-donors (Extensive-margin)" = 1
-        )) %>%
-        add_header_above(c(" " = 1, "Effective price" = 2)) %>%
+        add_header_above(c(" " = 1, "Effective price" = 1)) %>%
         group_rows("Excluded instruments", 1, 2, italic = TRUE, bold = FALSE) %>%
         group_rows("Covariates", 3, 4, italic = TRUE, bold = FALSE) %>%
-        column_spec(2:3, width = "18.75em") %>%
+        column_spec(2, width = "18.75em") %>%
         footnote(
           general_title = "",
           general = notes,
@@ -153,59 +132,42 @@ FirstPrice <- R6::R6Class("FirstPrice",
   ),
   private = list(
     stage1_mod = effective ~ applicable + ..stage2,
-    fe2sls_mod = list(
-      outcome ~ applicable + ..stage2,
-      outcome ~ effective + ..stage2,
-      outcome ~ ..stage2 | effective ~ applicable
-    ),
     main_reg = function(data, title = "", label = "", notes = "", font_size = 8) {
-      fit <- data %>%
-        group_by(type) %>%
-        nest() %>%
-        mutate(
-          mu = map_dbl(data, ~ with(., mean(outcome, na.rm = TRUE))),
-          fit_mod1 = map(data, ~ feols(private$fe2sls_mod[[1]], data = ., vcov = ~hhid)),
-          fit_mod2 = map(data, ~ feols(private$fe2sls_mod[[2]], data = ., vcov = ~hhid)),
-          fit_mod3 = map(data, ~ feols(private$fe2sls_mod[[3]], data = ., vcov = ~hhid))
-        ) %>%
-        select(-data) %>%
-        arrange(desc(type)) %>%
-        pivot_longer(
-          fit_mod1:fit_mod3,
-          names_to = c(".value", "model"),
-          names_pattern = "(.*)_(.*)"
-        )
+      mods <- list(
+        donate_ln ~ applicable + ..stage2,
+        donate_ln ~ effective + ..stage2,
+        donate_ln ~ ..stage2 | effective ~ applicable,
+        d_donate ~ applicable + ..stage2,
+        d_donate ~ effective + ..stage2,
+        d_donate ~ ..stage2 | effective ~ applicable
+      )
 
-      addtab <- fit %>%
-        mutate(
-          imp = map2(fit, mu, ~ implied_e(.x, .y)),
-          ivf = map_dbl(fit, ~ get_fitstat(., "ivf", "stat")),
-          wh = map_dbl(fit, ~ get_fitstat(., "wh", "p"))
-        ) %>%
-        select(-mu, -fit) %>%
-        unnest(imp) %>%
-        mutate_at(vars(estimate, estimate_se), list(~ ifelse(type == "extensive", ., ""))) %>%
-        mutate_at(vars(ivf, wh), list(~ ifelse(is.na(.), "", sprintf("\\num{%1.3f}", .)))) %>%
-        mutate_at(vars(ivf, wh), list(~ ifelse(. == "\\num{0.000}", "$<$ \\num{0.001}", .))) %>%
-        mutate(id = paste0(type, "_", model)) %>%
-        ungroup() %>%
-        select(-type, -model) %>%
-        pivot_longer(-id) %>%
-        pivot_wider(names_from = id, values_from = value) %>%
-        mutate(name = recode(
-          name,
-          "estimate" = "Estimate",
-          "estimate_se" = "",
-          "ivf" = "F-statistics of instrument",
-          "wh" = "Wu-Hausman test, p-value"
-        ))
+      fit_mods <- mods %>%
+        purrr::map(~ feols(., data = data, vcov = ~ hhid))
 
-      attr(addtab, "position") <- 9:12
+      dmu <- mean(data$d_donate, na.rm = TRUE)
+
+      imp <- fit_mods %>%
+        purrr::map(~ implied_e(., dmu)) %>%
+        purrr::map(~ pivot_longer(., everything())[, 2]) %>%
+        reduce(cbind)
+      imp[, 1:3] <- ""
+
+      ivf <- fit_mods %>%
+        sapply(function(x) get_fitstat(x, "ivf", "stat")) %>%
+        sapply(function(x) ifelse(is.na(x), "", sprintf("\\num{%1.3f}", x)))
+
+      addtab <- data.frame(rbind(imp, ivf))
+      addtab <- cbind(
+        term = c("Estimate", "", "F-statistics of instrument"),
+        addtab
+      )
+
+      attr(addtab, "position") <- 9:11
 
       if (label != "") label <- paste0("\\label{tab:", label, "}")
 
-      fit %>%
-        pull(fit) %>%
+      fit_mods %>%
         setNames(paste0("(", seq(length(.)), ")")) %>%
         modelsummary(
           title = paste0(title, label),
@@ -228,7 +190,7 @@ FirstPrice <- R6::R6Class("FirstPrice",
         group_rows("Implied price elasticity", 9, 10, italic = TRUE, bold = FALSE) %>%
         group_rows(
           "1st stage information (Excluded instrument: Applicable price)",
-          11, 12,
+          11, 11,
           bold = FALSE, italic = TRUE
         ) %>%
         column_spec(2:7, width = "5em") %>%
