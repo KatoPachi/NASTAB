3b2ba91346cc9da605897686c1c00583d3ac537f
Hiroki Kato <hkato.econ@gmail.com>
2024-07-15 14:00:39 +0900

    add taxable income in summary table

--- R/R6_SummaryData.r
+++ R/R6_SummaryData.r
@@ -15,7 +15,8 @@ SummaryData <- R6::R6Class("SummaryData", list(
 
     self$data %>%
       datasummary(
-        (`Annual disposal income (unit: 10,000KRW)` = after_tax_tinc) +
+          (`Annual taxable income (unit: 10,000KRW)` = taxable_tinc) +
+          (`Annual disposal income (unit: 10,000KRW)` = after_tax_tinc) +
           (`Simulated price` = price) +
           (`Annual chariatable giving (unit: 10,000KRW)` = donate) +
           (`Dummary of donation $>$ 0` = d_donate) +
